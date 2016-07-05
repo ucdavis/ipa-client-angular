21f0ad78246d60da8c2cc7374cf9b7dc4b04c070
Obada Kadri <okadri@ucdavis.edu>
2016-07-05 11:20:34 -0700

    Closes #44 Integrates Courses mock

---
+++ app/course/controllers/CourseCtrl.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ipaClientAngularApp.controller:CourseCtrl
+ * @description
+ * # CourseCtrl
+ * Controller of the ipaClientAngularApp
+ */
+courseApp.controller('CourseCtrl', ['$scope', '$rootScope', '$routeParams',
+		this.CourseCtrl = function ($scope, $rootScope, $routeParams) {
+			$scope.courseCode = $routeParams.courseCode;
+			$scope.year = $routeParams.year;
+			$scope.view = {};
+
+			$rootScope.$on('courseStateChanged', function (event, data) {
+				$scope.view.state = data;
+			});
+	}]);
+
+CourseCtrl.getPayload = function (authService, $route) {
+	authService.validate(localStorage.getItem('JWT'), $route.current.params.courseCode, $route.current.params.year).then(function () {
+		// return courseActionCreators.getInitialState($route.current.params.courseCode);
+		return;
+	});
+}
--- app/course/courseApp.js
+++ app/course/courseApp.js
@@ -1,1 +1,22 @@
-// Courses App
+window.courseApp = angular.module("courseApp", ["sharedApp", "ngRoute"]);
+
+courseApp.config(function ($routeProvider) {
+	return $routeProvider
+		.when("/:courseCode/:year", {
+			templateUrl: "CourseCtrl.html",
+			controller: "CourseCtrl",
+			resolve: {
+				payload: CourseCtrl.getPayload
+			}
+		})
+		.when("/", {
+			templateUrl: "CourseCtrl.html",
+			controller: "CourseCtrl",
+			resolve: {
+				payload: CourseCtrl.getPayload
+			}
+		})
+		.otherwise({
+			redirectTo: "/"
+		});
+});
