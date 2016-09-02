0876832991f6dbb537a06d1ec6f21793fc303978
Mark Diez <markediez@gmail.com>
2016-09-02 15:23:12 -0700

    Terms are loaded from DW to Summary page but code is not in the proper place

--- app/summary/controllers/SummaryCtrl.js
+++ app/summary/controllers/SummaryCtrl.js
@@ -12,29 +12,55 @@ summaryApp.controller('SummaryCtrl', ['$scope', '$routeParams', '$http',
 			$scope.workgroupId = $routeParams.workgroupId;
 			$scope.year = $routeParams.year;
 
-			// $http.get("").then(function(response) {
-			// 	$scope.data = response.data;
-			// 	console.log($scope.data);
-			// });
-			$scope.events = [{
-					'date': "8/21/2016",
-					'time': "12:00 AM",
-					'type': "school",
-					'title': "Fall Quarter Started"
-				}, {
-					'date': "8/12/2016",
-					'time': "1:42 PM",
-					'type': "notice",
-					'title': "Summer Session I Upload Available"
-				}, {
-					'date': "9/01/2016",
-					'time': "9:00 AM",
-					'type': "teaching_call",
-					'title': "Teaching Call 2017 Started",
-					'caption': "Send to Federation and Senate Instructors",
-					'link': "11" // probably would be the id
+			$http.get("https://beta.dw.dss.ucdavis.edu/terms?token=dssit").then(function(response) {
+				$scope.data = response.data;
+
+				// Only grab from this year
+				$scope.filtered = $scope.data.filter(function(term) {
+					var currentYear = new Date();
+					currentYear = currentYear.getFullYear() * 100;
+
+					// Only return for the current year and ignore term code 04
+					return (term.code >= currentYear && term.code != (currentYear + 4));
+				})
+
+				$scope.events = [];
+
+				var j = 0;
+				for (var i = 0; i < $scope.filtered.length; i++) {
+					var startDate = new Date($scope.filtered[i].beginDate);
+					var endDate = new Date($scope.filtered[i].endDate);
+
+					if ($scope.filtered[i].beginDate < Date.now()) {
+						$scope.events[j] = {
+							'longtime': startDate,
+							'date': startDate.toLocaleDateString(),
+							'time': startDate.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'}),
+							'title': $scope.filtered[i].code.getTermCodeDisplayName() + " Start",
+							'type': "school"
+						};
+
+						j++;
+					}
+
+					if ($scope.filtered[i].endDate < Date.now()) {
+						$scope.events[j] = {
+							'date': endDate.toLocaleDateString(),
+							'time': endDate.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'}),
+							'title': $scope.filtered[i].code.getTermCodeDisplayName() + " End",
+							'type': "school"
+						}
+
+						j++;
+
+					}
 				}
-			];
+
+				// Sort from most recent to least recent
+				$scope.events.sort(function(a,b) {
+				  return new Date(b.date).getTime() - new Date(a.date).getTime();
+				});
+			});
 }]);
 
 SummaryCtrl.authenticate = function (authService, $route, summaryActionCreators) {
