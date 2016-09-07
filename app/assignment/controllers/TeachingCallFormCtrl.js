746d053268f54c01c5f8fda2e6254c36db510b87
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2016-09-07 09:11:09 -0700

    Fix titles on assignment pages #309

--- app/assignment/controllers/TeachingCallFormCtrl.js
+++ app/assignment/controllers/TeachingCallFormCtrl.js
@@ -7,8 +7,9 @@
  * # TeachingCallFormCtrl
  * Controller of the ipaClientAngularApp
  */
-assignmentApp.controller('TeachingCallFormCtrl', ['$scope', '$rootScope', '$routeParams', '$timeout', 'assignmentActionCreators',
-		this.TeachingCallFormCtrl = function ($scope, $rootScope, $routeParams, $timeout, assignmentActionCreators) {
+assignmentApp.controller('TeachingCallFormCtrl', ['$scope', '$rootScope', '$window', '$routeParams', '$timeout', 'assignmentActionCreators',
+		this.TeachingCallFormCtrl = function ($scope, $rootScope, $window, $routeParams, $timeout, assignmentActionCreators) {
+			$window.document.title = "Teaching Call";
 			$scope.workgroupId = $routeParams.workgroupId;
 			$scope.year = $routeParams.year;
 			$scope.nextYear = (parseInt($scope.year) + 1).toString().slice(-2);
