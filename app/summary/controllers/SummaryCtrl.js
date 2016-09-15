040b735196cf160034fb19972bdc26e35cde4ba3
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2016-09-15 11:09:21 -0700

    SummaryView: Replace mock data in instructor view and improve UI #249

--- app/summary/controllers/SummaryCtrl.js
+++ app/summary/controllers/SummaryCtrl.js
@@ -30,6 +30,28 @@ summaryApp.controller('SummaryCtrl', ['$scope', '$routeParams', '$rootScope',
 					$scope.sharedState.isAcademicPlanner = true;
 				}
 			});
+
+			$scope.getTermName = function(term) {
+				var endingYear = "";
+				if (term.length == 6) {
+					endingYear = term.substring(0,4);
+					term = term.slice(-2);
+				}
+
+				termNames = {
+					'05': 'Summer Session 1',
+					'06': 'Summer Special Session',
+					'07': 'Summer Session 2',
+					'08': 'Summer Quarter',
+					'09': 'Fall Semester',
+					'10': 'Fall Quarter',
+					'01': 'Winter Quarter',
+					'02': 'Spring Semester',
+					'03': 'Spring Quarter'
+				};
+
+				return termNames[term] + " " + endingYear;
+			};
 }]);
 
 SummaryCtrl.authenticate = function (authService, $route, summaryActionCreators) {
