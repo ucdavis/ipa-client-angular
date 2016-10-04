145dc524e2d3a6d981a836fa7cd8ae8e00584c24
Obada Kadri <okadri@ucdavis.edu>
2016-10-04 10:35:02 -0700

    Closes #431 Updates workgroup person search style

--- app/shared/directives/spinner.js
+++ app/shared/directives/spinner.js
@@ -9,7 +9,8 @@ sharedApp.directive("spinner", this.spinner = function () {
 		link: function (scope, element, attrs) {
 			var target = $('.spinner-container', element);
 			var text = attrs.text ? ' &nbsp; ' + attrs.text : '';
-			target.html('<img src="/images/ajax-loader.gif" />' + text);
+			var style = attrs.size ? 'style="width:' + attrs.size + 'px; height:' + attrs.size + 'px; "' : '';
+			target.html('<img src="/images/ajax-loader.gif" ' + style + ' />' + text);
 		}
 	};
 });
