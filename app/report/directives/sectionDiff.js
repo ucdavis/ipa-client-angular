290fd43c709a8d6ffaa8db475aec47726dd1063e
Obada Kadri <okadri@ucdavis.edu>
2016-11-09 10:59:17 -0800

    #622 changes sectionDiff directive for the new dwChanges modifications

--- app/report/directives/sectionDiff.js
+++ app/report/directives/sectionDiff.js
@@ -6,6 +6,9 @@ reportApp.directive("sectionDiff", this.sectionDiff = function () {
 	return {
 		restrict: "A",
 		templateUrl: 'sectionDiff.html',
-		replace: true
+		replace: true,
+		link: function (scope, element, attrs) {
+			scope.section = scope.view.state.sections.list[scope.sectionId];
+		}
 	};
 });
