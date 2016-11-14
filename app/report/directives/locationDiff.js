a01258dcd268380927293a3344e0c5ee20259036
Obada Kadri <okadri@ucdavis.edu>
2016-11-14 13:48:41 -0800

    Closes #596 Makes Location actions functional

--- app/report/directives/locationDiff.js
+++ app/report/directives/locationDiff.js
@@ -2,10 +2,20 @@
  * example:
  * <location-diff></location-diff>
  */
-reportApp.directive("locationDiff", this.locationDiff = function(reportActionCreators) {
-    return {
-        restrict: "E",
-        templateUrl: 'locationDiff.html',
-        replace: true
-    };
+reportApp.directive("locationDiff", this.locationDiff = function (reportActionCreators) {
+	return {
+		restrict: "E",
+		templateUrl: 'locationDiff.html',
+		replace: true,
+		link: function (scope, element, attrs) {
+			scope.updateBannerLocation = function (bannerLocation) {
+				var activity = {
+					id: scope.activity.id,
+					typeCode: scope.activity.typeCode,
+					bannerLocation: bannerLocation
+				};
+				reportActionCreators.updateActivity(activity, 'bannerLocation');
+			};
+		}
+	};
 });
