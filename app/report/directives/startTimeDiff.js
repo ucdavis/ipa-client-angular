ee36aff5f46d40adef3794017abed3bbb35b8504
Obada Kadri <okadri@ucdavis.edu>
2016-11-14 13:34:10 -0800

    Closes #594 Makes StartTime actions functional

--- app/report/directives/startTimeDiff.js
+++ app/report/directives/startTimeDiff.js
@@ -6,6 +6,16 @@ reportApp.directive("startTimeDiff", this.startTimeDiff = function (reportAction
 	return {
 		restrict: "E",
 		templateUrl: 'startTimeDiff.html',
-		replace: true
+		replace: true,
+		link: function (scope, element, attrs) {
+			scope.updateStartTime = function (startTime) {
+				var activity = {
+					id: scope.activity.id,
+					typeCode: scope.activity.typeCode,
+					startTime: moment(startTime, "HHmm").format("HH:mm:ss")
+				};
+				reportActionCreators.updateActivity(activity, 'startTime');
+			};
+		}
 	};
 });
