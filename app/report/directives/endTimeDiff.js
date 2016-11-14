8cf93134eaf8fc79d866d850445e5de94176a6dd
Obada Kadri <okadri@ucdavis.edu>
2016-11-14 13:38:07 -0800

    Closes #595 Makes EndTime actions functional

--- app/report/directives/endTimeDiff.js
+++ app/report/directives/endTimeDiff.js
@@ -6,6 +6,16 @@ reportApp.directive("endTimeDiff", this.endTimeDiff = function (reportActionCrea
 	return {
 		restrict: "E",
 		templateUrl: 'endTimeDiff.html',
-		replace: true
+		replace: true,
+		link: function (scope, element, attrs) {
+			scope.updateEndTime = function (endTime) {
+				var activity = {
+					id: scope.activity.id,
+					typeCode: scope.activity.typeCode,
+					endTime: moment(endTime, "HHmm").format("HH:mm:ss")
+				};
+				reportActionCreators.updateActivity(activity, 'endTime');
+			};
+		}
 	};
 });
