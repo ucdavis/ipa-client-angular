b8bcef654c7f9cb9dc993257ce9ea6f3dbdaa9e7
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2016-11-15 14:15:05 -0800

    InstructionalSupport Call Config: Make People selector changes reflect in the UI and in the summary

--- app/instructionalSupport/supportCallStatus/controllers/ModalAddSupportCallCtrl.js
+++ app/instructionalSupport/supportCallStatus/controllers/ModalAddSupportCallCtrl.js
@@ -1,6 +1,30 @@
 instructionalSupportApp.controller('ModalAddSupportCallCtrl', this.ModalAddSupportCallCtrl = function($scope, $rootScope, $uibModalInstance, instructionalSupportAssignmentActionCreators, sectionGroupId, appointmentType) {
 
+	$scope.phdPool = [
+		{id: 1, displayName: "John Smith", group: "phd", enabled: true},
+		{id: 2, displayName: "Jenny Garcia", group: "phd", enabled: true},
+		{id: 3, displayName: "Lloyd Wheeler", group: "phd", enabled: true}
+	];
+
+	$scope.mastersPool = [
+		{id: 4, displayName: "Bobbi Hale", group: "masters", enabled: true},
+		{id: 5, displayName: "Jeremy Phillips", group: "masters", enabled: true}
+	];
+
+	$scope.instructionalSupportPool = [
+		{id: 6, displayName: "Joel Dobris", group: "instructionalSupport", enabled: true},
+		{id: 7, displayName: "Grant Wallace", group: "instructionalSupport", enabled: true}
+	];
+
+	$scope.instructorPool = [
+		{id: 8, displayName: "Dave MacKinnon", group: "instructor", enabled: true},
+		{id: 9, displayName: "Jenny Green", group: "instructor", enabled: true}
+	];
+
 	$scope.supportCallConfigData = {mode: "instructor", displayPage: 1};
+	$scope.supportCallConfigData.phdParticipants = false;
+	$scope.supportCallConfigData.mastersParticipants = false;
+	$scope.supportCallConfigData.participantPool = [];
 
 	$scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate', 'yyyy-MM-dd'];
 	$scope.format = $scope.formats[4];
@@ -31,39 +55,99 @@ instructionalSupportApp.controller('ModalAddSupportCallCtrl', this.ModalAddSuppo
 
 	$scope.toggleInstructor = function () {
 		$scope.supportCallConfigData.instructorParticipants = true;
+		addGroupToPool("instructor");
 
 		$scope.supportCallConfigData.phdParticipants = false;
+		removeGroupFromPool("phd");
 		$scope.supportCallConfigData.mastersParticipants = false;
+		removeGroupFromPool("masters");
 		$scope.supportCallConfigData.instructionalSupportParticipants = false;
+		removeGroupFromPool("instructionalSupport");
 	};
 
 	$scope.togglePhd = function () {
 		$scope.supportCallConfigData.instructorParticipants = false;
+		removeGroupFromPool("instructor");
 
 		if ($scope.supportCallConfigData.phdParticipants) {
 			$scope.supportCallConfigData.phdParticipants = false;
+			removeGroupFromPool("phd");
 		} else {
 			$scope.supportCallConfigData.phdParticipants = true;
+			addGroupToPool("phd");
 		}
 	};
 
 	$scope.toggleMasters = function () {
 		$scope.supportCallConfigData.instructorParticipants = false;
+		removeGroupFromPool("instructor");
 
 		if ($scope.supportCallConfigData.mastersParticipants) {
 			$scope.supportCallConfigData.mastersParticipants = false;
+			removeGroupFromPool("masters");
 		} else {
 			$scope.supportCallConfigData.mastersParticipants = true;
+			addGroupToPool("masters");
 		}
 	};
 
 	$scope.toggleInstructionalSupport = function () {
 		$scope.supportCallConfigData.instructorParticipants = false;
+		removeGroupFromPool("instructor");
 
 		if ($scope.supportCallConfigData.instructionalSupportParticipants) {
 			$scope.supportCallConfigData.instructionalSupportParticipants = false;
+			removeGroupFromPool("instructionalSupport");
 		} else {
 			$scope.supportCallConfigData.instructionalSupportParticipants = true;
+			addGroupToPool("instructionalSupport");
+		}
+	};
+
+	// Looks in the current participant pool for any individuals from the specified group, and removes them
+	removeGroupFromPool = function (participantGroup) {
+		for (var i = $scope.supportCallConfigData.participantPool.length - 1; i >= 0; i--) {
+			var participant = $scope.supportCallConfigData.participantPool[i];
+
+			if (participant.group == participantGroup) {
+				$scope.supportCallConfigData.participantPool.splice(i, 1);
+			}
+		}
+	};
+
+	// Adds all users in the specified group to the participant pool
+	addGroupToPool = function (participantGroup) {
+		var groupToAdd = {};
+
+		switch(participantGroup) {
+			case "phd":
+				groupToAdd = $scope.phdPool;
+				break;
+			case "masters":
+				groupToAdd = $scope.mastersPool;
+				break;
+			case "instructionalSupport":
+				groupToAdd = $scope.instructionalSupportPool;
+				break;
+			case "instructor":
+				groupToAdd = $scope.instructorPool;
+				break;
+		}
+
+		for (var i = 0; i < groupToAdd.length; i++) {
+			var participant = groupToAdd[i];
+			participant.enabled = true;
+			$scope.supportCallConfigData.participantPool.push(participant);
+		}
+
+		console.log($scope.supportCallConfigData.participantPool);
+	};
+
+	$scope.toggleEnabled = function (participant) {
+		if (participant.enabled) {
+			participant.enabled = false;
+		} else {
+			participant.enabled = true;
 		}
 	};
 
