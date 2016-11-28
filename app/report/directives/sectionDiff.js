1f8e231f0a3571af8198514c6ed5536558376adf
Obada Kadri <okadri@ucdavis.edu>
2016-11-28 11:17:34 -0800

    Closes #668 Click event propagates to parents if the chald has no changes

--- app/report/directives/sectionDiff.js
+++ app/report/directives/sectionDiff.js
@@ -14,20 +14,35 @@ reportApp.directive("sectionDiff", this.sectionDiff = function (reportActionCrea
 				reportActionCreators.createBannerToDoItem(scope.section.id, property, childUniqueKey, childProperty);
 			};
 
-			scope.setActiveChangeAction = function (actionKey, event) {
-				// Prevent click event from going up to parent divs like activity or section
-				event.stopPropagation();
+			scope.setActiveChangeAction = function (event, object, index, property) {
+				property = property || '';
+				var actionKey = object.uniqueKey + index + property;
 
 				if (scope.view.activeChangeAction == actionKey) {
 					scope.view.activeChangeAction = '';
 				} else {
 					scope.view.activeChangeAction = actionKey;
 				}
+
+				// Prevent click event from going up to parent divs like activity or section if it has changes
+				if (hasChanges(object, property)) {
+					event.stopPropagation();
+				}
 			};
 
 			scope.deleteSection = function (section) {
 				reportActionCreators.deleteSection(section);
 			};
+
+			function hasChanges(object, property) {
+				if (property) {
+					// Property of an object, hence look at the dwChanges object
+					return object.dwChanges && object.dwChanges[property] && !object.dwChanges[property].isToDo;
+				} else {
+					// An entire object (section, activity, instructor), look for noLocal or noRemote
+					return (object.noLocal || object.noRemote) && !object.isToDo;
+				}
+			}
 		}
 	};
 });
