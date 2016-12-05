b8247ff3efc01295eb3b886ac938ec5c284ceef6
Obada Kadri <okadri@ucdavis.edu>
2016-12-05 13:48:10 -0800

    Closes #684 Resets the sectionGroup details after deleting it

--- app/course/services/courseStateService.js
+++ app/course/services/courseStateService.js
@@ -208,6 +208,10 @@ courseApp.service('courseStateService', function ($rootScope, $log, Course, Term
 					var sectionGroupIndex = sectionGroups.ids.indexOf(action.payload.sectionGroup.id);
 					sectionGroups.ids.splice(sectionGroupIndex, 1);
 					delete sectionGroups.list[action.payload.sectionGroup.id];
+					sectionGroups.selectedSectionGroup = new SectionGroup({
+						courseId: action.payload.sectionGroup.courseId,
+						termCode: action.payload.sectionGroup.termCode
+					});
 					return sectionGroups;
 				case UPDATE_SECTION_GROUP:
 					sectionGroups.list[action.payload.sectionGroup.id] = new SectionGroup(action.payload.sectionGroup);
