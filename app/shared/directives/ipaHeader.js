c43e9c6c11b91307fb1a05378e5426d5b5623070
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-03-16 15:45:45 -0700

    Make impersonate functional in new header mock #795

--- app/shared/directives/ipaHeader.js
+++ app/shared/directives/ipaHeader.js
@@ -1,7 +1,17 @@
-sharedApp.directive('ipaHeader', function() {
+sharedApp.directive('ipaHeader', function($location, $rootScope, authService) {
 	return {
 		restrict: 'E', // Use this via an element selector <dss-modal></dss-modal>
 		templateUrl: 'ipaHeader.html', // directive html found here:
-		replace: true // Replace with the template below
+		replace: true, // Replace with the template below
+		link: function (scope, element, attrs) {
+
+			scope.impersonate = function(loginId) {
+				authService.impersonate(loginId);
+			};
+
+			scope.unImpersonate = function() {
+				authService.unimpersonate();
+			};
+		}
 	};
 });
