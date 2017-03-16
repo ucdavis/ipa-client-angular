a32f018f5341f188e03cddd9453d02abc04a3e92
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-03-16 15:55:09 -0700

    Integrate new header into Courses page #795

--- app/shared/directives/ipaHeader.js
+++ app/shared/directives/ipaHeader.js
@@ -4,6 +4,7 @@ sharedApp.directive('ipaHeader', function($location, $rootScope, authService) {
 		templateUrl: 'ipaHeader.html', // directive html found here:
 		replace: true, // Replace with the template below
 		link: function (scope, element, attrs) {
+			scope.pageTitle = attrs.pageTitle;
 
 			scope.impersonate = function(loginId) {
 				authService.impersonate(loginId);
