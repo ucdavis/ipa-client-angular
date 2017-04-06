e9a41963797d22e9c257e8a351f450046973cd15
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-04-06 10:52:58 -0700

    IpaHeader: Add support for optional page specific ui elements #834

--- app/shared/directives/ipaHeader.js
+++ app/shared/directives/ipaHeader.js
@@ -3,6 +3,7 @@ sharedApp.directive('ipaHeader', function($window, $location, $rootScope, authSe
 		restrict: 'E', // Use this via an element selector <dss-modal></dss-modal>
 		templateUrl: 'ipaHeader.html', // directive html found here:
 		replace: true, // Replace with the template below
+		transclude: true,
 		link: function (scope, element, attrs) {
 			scope.pageTitle = attrs.pageTitle;
 
