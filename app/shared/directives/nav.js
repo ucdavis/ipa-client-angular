b46446f621d4fdd24fce8f4c1414c59a04e30f81
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-04-07 14:16:21 -0700

    Give Biomedical Engineering access to test the support call module

--- app/shared/directives/nav.js
+++ app/shared/directives/nav.js
@@ -36,6 +36,8 @@ sharedApp.directive("nav", this.nav = function ($location, $rootScope, authServi
 
 				// Add 'Chemistry'
 				participatingWorkgroupIds.push("67");
+				// Add 'Biomedical Engineering'
+				participatingWorkgroupIds.push("15");
 
 				var currentWorkgroupId = scope.sharedState.workgroup.id;
 				var index = participatingWorkgroupIds.indexOf(currentWorkgroupId);
