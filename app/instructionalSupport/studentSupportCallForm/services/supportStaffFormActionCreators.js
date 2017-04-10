1cc37e9d47c35e079d58e298e9ae04b491c1fae2
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-04-10 01:58:33 -0700

    SupportStaffSupportCallForm: Updating state to separate state and viewState #939

---
+++ app/instructionalSupport/studentSupportCallForm/services/supportStaffFormActionCreators.js
@@ -0,0 +1,77 @@
+instructionalSupportApp.service('supportStaffFormActionCreators', function ($rootScope, $window, supportStaffFormService, supportStaffFormStateService) {
+	return {
+		getInitialState: function (workgroupId, year, termShortCode) {
+			supportStaffFormService.getInitialState(workgroupId, year, termShortCode).then(function (payload) {
+				var action = {
+					type: INIT_STATE,
+					payload: payload,
+					year: year
+				};
+				supportStaffFormStateService.reduce(action);
+			}, function (err) {
+				$rootScope.$emit('toast', { message: "Something went wrong. Please try again.", type: "ERROR" });
+			});
+		},
+		addStudentPreference: function (preference) {
+			supportStaffFormService.addStudentPreference(preference).then(function (payload) {
+				$rootScope.$emit('toast', { message: "Added Preference", type: "SUCCESS" });
+				var action = {
+					type: ADD_STUDENT_PREFERENCE,
+					payload: payload
+				};
+				supportStaffFormStateService.reduce(action);
+			}, function (err) {
+				$rootScope.$emit('toast', { message: "Something went wrong. Please try again.", type: "ERROR" });
+			});
+		},
+		updateSupportCallResponse: function (supportCallResponse) {
+			supportStaffFormService.updateSupportCallResponse(supportCallResponse).then(function (payload) {
+				$rootScope.$emit('toast', { message: "Updated preferences", type: "SUCCESS" });
+				var action = {
+					type: UPDATE_SUPPORT_CALL_RESPONSE,
+					payload: payload
+				};
+				supportStaffFormStateService.reduce(action);
+			}, function (err) {
+				$rootScope.$emit('toast', { message: "Something went wrong. Please try again.", type: "ERROR" });
+			});
+		},
+		deleteStudentPreference: function (preference) {
+			supportStaffFormService.deleteStudentPreference(preference.id).then(function (payload) {
+				$rootScope.$emit('toast', { message: "Removed Preference", type: "SUCCESS" });
+				var action = {
+					type: DELETE_STUDENT_PREFERENCE,
+					payload: preference
+				};
+				supportStaffFormStateService.reduce(action);
+			}, function (err) {
+				$rootScope.$emit('toast', { message: "Something went wrong. Please try again.", type: "ERROR" });
+			});
+		},
+		submitPreferences: function (supportCallResponse, workgroupId, year) {
+			supportStaffFormService.updateSupportCallResponse(supportCallResponse).then(function (payload) {
+				$rootScope.$emit('toast', { message: "Updated preferences", type: "SUCCESS" });
+				var studentSummaryUrl = "/summary/" + workgroupId + "/" + year + "?mode=instructionalSupport";
+				$window.location.href = studentSummaryUrl;
+			}, function (err) {
+				$rootScope.$emit('toast', { message: "Something went wrong. Please try again.", type: "ERROR" });
+			});
+		},
+		updatePreferencesOrder: function (preferenceIds, scheduleId, termCode) {
+			supportStaffFormService.updatePreferencesOrder(preferenceIds, scheduleId, termCode).then(function (payload) {
+				$rootScope.$emit('toast', { message: "Updated preferences", type: "SUCCESS" });
+				var action = {
+					type: UPDATE_PREFERENCES_ORDER,
+					payload: payload
+				};
+				supportStaffFormStateService.reduce(action);
+			}, function (err) {
+				$rootScope.$emit('toast', { message: "Something went wrong. Please try again.", type: "ERROR" });
+			});
+		},
+		pretendToastMessage: function () {
+			$rootScope.$emit('toast', { message: "Updated preferences", type: "SUCCESS" });
+		}
+
+	};
+});
---
+++ app/instructionalSupport/studentSupportCallForm/services/supportStaffFormSelectors.js
@@ -0,0 +1,150 @@
+/*
+	Selectors are pure javascript functions that translate the normalized state into nested objects for the view
+*/
+instructionalSupportApp.service('supportStaffFormSelectors', function () {
+	return {
+/*
+		misc
+		supportCallResponse
+		supportStaffPreferences
+			preference
+				supportStaffPreference
+				courseData
+				sectionGroupData
+		potentialPreferences (supportAssignments with more data)
+			(split into reader, ai, ta groups)
+				supportAssignment
+				courseData
+				sectionGroupData
+*/
+		generatePreferences: function (preferences, courses, sectionGroups) {
+			var self = this;
+
+			var newPreferences = [];
+
+			preferences.ids.forEach( function(preferenceId) {
+				var preference = preferences.list[preferenceId];
+
+				preference = self.addSectionGroupData(preference, sectionGroups);
+				preference = self.addCourseData(preference, courses);
+
+				newPreferences.push(preference);
+			});
+
+			return newPreferences;
+		},
+		generatePotentialPreferences: function (supportAssignments, courses, sectionGroups, preferences, supportCallResponse) {
+			var self = this;
+
+			var potentialPreferences = {};
+			potentialPreferences.readers = [];
+			potentialPreferences.associateInstructors = [];
+			potentialPreferences.teachingAssistants = [];
+
+			supportAssignments.ids.forEach( function(supportAssignmentId) {
+				var supportAssignment = supportAssignments.list[supportAssignmentId];
+
+				supportAssignment = self.addSectionGroupData(supportAssignment, sectionGroups);
+				supportAssignment = self.addCourseData(supportAssignment, courses);
+
+				// Ensure assignment is the right type
+				if (self.isAssignmentRelevantToSupportCall(supportAssignment, supportCallResponse) == false) {
+					return;
+				}
+
+				// Ensure assignment does not match an existing preference
+				if (self.isPotentialPreference(supportAssignment, preferences) == false) {
+					return;
+				}
+
+				// Identify relevant potential preferenceGroup
+				var group = null;
+				if (supportAssignment.appointmentType == "reader") {
+					group = potentialPreferences.readers;
+				}
+				if (supportAssignment.appointmentType == "teachingAssistant") {
+					group = potentialPreferences.teachingAssistants;
+				}
+				if (supportAssignment.appointmentType == "associateInstructor") {
+					group = potentialPreferences.associateInstructors;
+				}
+
+				// Ensure assignment does not match an existing potentialPreference
+				for (var i = 0; i < group.length; i++) {
+					var potentialPreference = group[i];
+
+					if (potentialPreference.courseNumber == supportAssignment.courseNumber
+					&& potentialPreference.subjectCode == supportAssignment.subjectCode
+					&& potentialPreference.sequencePattern == supportAssignment.sequencePattern
+					&& potentialPreference.appointmentPercentage == supportAssignment.appointmentPercentage) {
+						return;
+					}
+				}
+
+				// Add assignment
+				group.push(supportAssignment);
+			});
+
+			return potentialPreferences;
+		},
+		// Determine if a supportAssignment has already been selected as a preference
+		isPotentialPreference: function(supportAssignment, preferences) {
+			for (var i = 0; i < preferences.ids.length; i++) {
+				var preferenceId = preferences.ids[i];
+				var preference = preferences.list[preferenceId];
+
+				if (preference.subjectCode == supportAssignment.subjectCode
+					&& preference.courseNumber == supportAssignment.courseNumber
+					&& preference.type == supportAssignment.appointmentType
+					&& preference.sequencePattern == supportAssignment.sequencePattern) {
+
+					return false;
+				}
+			}
+
+			return true;
+		},
+		// Returns true if assignment type matches a type being collected in the support call
+		isAssignmentRelevantToSupportCall: function(supportAssignment, supportCallResponse) {
+			if (supportCallResponse.collectReaderPreferences && supportAssignment.appointmentType == "reader") {
+				return true;
+			}
+			if (supportCallResponse.collectTeachingAssistantPreferences && supportAssignment.appointmentType == "teachingAssistant") {
+				return true;
+			}
+			if (supportCallResponse.collectAssociateInstructorPreferences && supportAssignment.appointmentType == "associateInstructor") {
+				return true;
+			}
+
+			return false;
+		},
+		// Blend the relevant course data
+		addCourseData: function (entity, courses) {
+			courses.ids.forEach( function (courseId) {
+				var course = courses.list[courseId];
+
+				if (entity.courseId == course.id) {
+					entity.subjectCode = course.subjectCode;
+					entity.sequencePattern = course.sequencePattern;
+					entity.courseNumber = course.courseNumber;
+					entity.title = course.title;
+					entity.units = course.unitsLow;
+				}
+			});
+
+			return entity;
+		},
+		// Blend the relevant course data
+		addSectionGroupData: function (entity, sectionGroups) {
+			sectionGroups.ids.forEach( function (sectionGroupId) {
+				var sectionGroup = sectionGroups.list[sectionGroupId];
+
+				if (entity.sectionGroupId == sectionGroup.id) {
+					entity.courseId = sectionGroup.courseId;
+				}
+			});
+
+			return entity;
+		},
+	};
+});
