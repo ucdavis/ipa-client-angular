dcf14ef898f7f5e50a092ba076fb1bf62ab728bb
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-05-24 10:12:36 -0700

    Reformat term selection to dropdown #969

---
+++ app/shared/directives/ipaTermSelectorDropdown/ipaTermSelectorDropdown.js
@@ -0,0 +1,64 @@
+sharedApp.directive('ipaTermSelectorDropdown', function($window, $location, $routeParams, $rootScope, authService, Term) {
+	return {
+		restrict: 'E', // Use this via an element selector <dss-modal></dss-modal>
+		templateUrl: 'ipaTermSelectorDropdown.html', // directive html found here:
+		replace: true, // Replace with the template below
+		scope: {
+			termStates: '='
+		},
+		link: function (scope, element, attrs) {
+			scope.year = $routeParams.year;
+			scope.workgroupId = $routeParams.workgroupId;
+			scope.termShortCode = $routeParams.termShortCode;
+			scope.currentEndHref = $location.path().split('/').pop();
+
+			// Term navigation is disabled in read only mode
+			scope.readOnlyMode = false;
+
+			// Instructor/SupportStaff forms should not allow for term navigation
+			if (scope.currentEndHref == "instructorSupportCallForm"
+			|| scope.currentEndHref == "studentSupportCallForm") {
+					scope.readOnlyMode = true;
+			}
+
+			scope.termDefinitions = Term.prototype.generateTable(scope.year);
+
+			scope.getScheduleTerms = function () {
+				if (!scope.termStates) { return; }
+
+				var activeTerms = scope.termStates.map(function (termState) {
+					return termState.termCode;
+				});
+				var scheduleTerms = scope.termDefinitions.filter(function (term) {
+					return activeTerms.indexOf(term.code) >= 0;
+				});
+
+				return scheduleTerms;
+			};
+
+			scope.scheduleTerms = scope.getScheduleTerms();
+
+			scope.scheduleTerms.forEach( function(term) {
+				if (term.shortCode == scope.termShortCode) {
+					scope.currentTermDescription = term.description;
+				}
+			});
+
+			if (scope.currentTermDescription == null) {
+				scope.readOnlyMode = true;
+				scope.currentTermDescription = "Annual";
+			}
+
+			scope.gotoTerm = function (newTermShortCode) {
+				var url = $location.absUrl();
+
+				n = url.lastIndexOf(scope.termShortCode);
+				if (n > -1) {
+					url = url.substring(0, n) + newTermShortCode + url.substring(n+2, url.length);
+				}
+
+				$window.location.href = url;
+			};
+		} // End Link
+	};
+});
