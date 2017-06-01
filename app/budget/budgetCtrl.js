40bc0d36a8d81448678b82feb4e018ec737f2c90
shanglu9425 <shanglu9425ll@hotmail.com>
2017-06-01 13:50:13 -0700

    - added budget html structure. - added budget css styles. - added budget JS  to show interaction.

--- app/budget/budgetCtrl.js
+++ app/budget/budgetCtrl.js
@@ -14,3 +14,21 @@ BudgetCtrl.getPayload = function ($route, $window) {
 	console.log("I am the budget payload");
 	return null;
 };
+
+
+$(function() {
+    $(".description-row").find(".row").hide();
+    $("table").click(function(event) {
+        event.stopPropagation();
+        var $target = $(event.target);
+        $target.closest("tr").next().find(".row").slideToggle();
+    });
+
+
+	$('.form-group').find("span").bind('dblclick',function(){
+		$(this).attr('contentEditable',true);
+    });
+
+
+});
+
