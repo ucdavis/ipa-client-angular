1713692f6e519ffb2af56eb2056a2550a2df46a6
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-06-12 00:13:57 -0700

    Grunt: temporarily reverting the use of fat arrow syntax

--- app/teachingCall/teachingCallForm/services/teachingCallFormStateService.js
+++ app/teachingCall/teachingCallForm/services/teachingCallFormStateService.js
@@ -91,7 +91,7 @@ teachingCallApp.service('teachingCallFormStateService', function (
 					// Calculate termSelection UI
 					pageState.termSelection = [];
 
-					pageState.terms.forEach( (term) => {
+					pageState.terms.forEach( function(term) {
 						newTerm = {};
 						newTerm.description = term.termDescription;
 						newTerm.isSelected = false;
