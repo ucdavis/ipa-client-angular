7507a7250bb260f229aa0e404713bbedd88d5093
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-07-03 09:40:04 -0700

    BudgetView: Add budget scenario form properly updates source on selection

--- app/budget/directives/addBudgetScenario/addBudgetScenario.js
+++ app/budget/directives/addBudgetScenario/addBudgetScenario.js
@@ -11,14 +11,22 @@ budgetApp.directive("addBudgetScenario", this.addBudgetScenario = function ($roo
 			scope.newBudgetScenario = {};
 			scope.newBudgetScenario.name = "";
 			scope.newBudgetScenario.budgetScenarioId = 0;
+			scope.newBudgetScenario.description = "Schedule Data";
+
 			scope.budgetId = attrs.budgetId;
 
+			scope.selectBudgetScenario = function(budgetScenario) {
+				if (budgetScenario == null) {
+					scope.newBudgetScenario.budgetScenarioId = 0;
+					scope.newBudgetScenario.description = "Schedule Data";
+				} else {
+					scope.newBudgetScenario.budgetScenarioId = budgetScenario.id;
+					scope.newBudgetScenario.description = budgetScenario.name;
+				}
+			};
 			scope.submitBudgetScenarioForm = function () {
 				budgetActions.createBudgetScenario(scope.newBudgetScenario, scope.budgetId);
 			};
-			scope.cancel = function () {
-				
-			}
 		} // end link
 	};
 });
