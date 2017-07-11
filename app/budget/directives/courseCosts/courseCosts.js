e75f8bc5ea89b488e0cff83df2f8baf77cc494e0
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-07-11 15:20:26 -0700

    BudgteView: Setting up courseCosts layout

--- app/budget/directives/courseCosts/courseCosts.js
+++ app/budget/directives/courseCosts/courseCosts.js
@@ -1,11 +1,15 @@
-budgetApp.directive("courseCosts", this.courseCosts = function ($rootScope) {
+budgetApp.directive("courseCosts", this.courseCosts = function ($rootScope, budgetActions) {
 	return {
 		restrict: 'E',
 		templateUrl: 'courseCosts.html',
 		replace: true,
-		scope: false,
+		scope: {
+			activeBudgetScenario: '<'
+		},
 		link: function (scope, element, attrs) {
-
+			scope.toggleCourseCostsSection = function() {
+				budgetActions.toggleCourseCostsSection();
+			};
 		} // end link
 	};
 });
