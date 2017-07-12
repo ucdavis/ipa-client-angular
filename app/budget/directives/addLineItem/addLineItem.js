44c54bbb6818e53f597f9b25b986821304378a25
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-07-12 03:28:37 -0700

    BudgetView: Refactor addLineItem modal to isolate scope

--- app/budget/directives/addLineItem/addLineItem.js
+++ app/budget/directives/addLineItem/addLineItem.js
@@ -12,15 +12,13 @@ budgetApp.directive("addLineItem", this.addLineItem = function ($rootScope, budg
 		link: function (scope, element, attrs) {
 			scope.newLineItem = {};
 			scope.newLineItem.categoryDescription = "Select a category";
-			scope.newLineItem.budgetScenarioId = scope.state.activeBudgetScenario.id;
 
 			scope.selectLineItemCategory = function(category) {
 				scope.newLineItem.lineItemCategoryId = category.id;
 				scope.newLineItem.categoryDescription = category.description;
 			};
 			scope.submitLineItemForm = function () {
-				// TODO: ipa-modal should ideally be in charge of modal UI changes, and this directive should not have to worry about cleanup
-				$('body').css('overflow-y','visible');
+				scope.newLineItem.budgetScenarioId = scope.state.activeBudgetScenario.id;
 
 				budgetActions.createLineItem(scope.newLineItem, scope.state.activeBudgetScenario.id);
 			};
