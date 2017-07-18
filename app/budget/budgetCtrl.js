d33087487007c5af240153037b9a7a7a18780591
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-07-18 09:36:36 -0700

    BudgetView: Fixing code formatting

--- app/budget/budgetCtrl.js
+++ app/budget/budgetCtrl.js
@@ -1,10 +1,3 @@
-/**
- * @ngdoc function
- * @name ipaClientAngularApp.controller:AssignmentCtrl
- * @description
- * # AssignmentCtrl
- * Controller of the ipaClientAngularApp
- */
 budgetApp.controller('BudgetCtrl', ['$scope', '$rootScope', '$window', '$location', '$routeParams', '$uibModal', '$timeout',
 	this.BudgetCtrl = function ($scope, $rootScope, $window, $location, $routeParams, $timeout) {
 		$scope.workgroupId = $routeParams.workgroupId;
@@ -27,8 +20,6 @@ budgetApp.controller('BudgetCtrl', ['$scope', '$rootScope', '$window', '$locatio
 			} else {
 				localStorage.removeItem('selectedTerm');
 			}
-
-
 		});
 
 		$scope.openLineItemModal = function() {
@@ -46,10 +37,11 @@ budgetApp.controller('BudgetCtrl', ['$scope', '$rootScope', '$window', '$locatio
 
 BudgetCtrl.getPayload = function (authService, $route, $window, budgetActions) {
 	authService.validate(localStorage.getItem('JWT'), $route.current.params.workgroupId, $route.current.params.year).then(function () {
-		budgetActions.getInitialState($route.current.params.workgroupId,
-																	$route.current.params.year,
-																	localStorage.getItem('selectedBudgetScenarioId'),
-																	localStorage.getItem('selectedTerm'));
+		budgetActions.getInitialState(
+			$route.current.params.workgroupId,
+			$route.current.params.year,
+			localStorage.getItem('selectedBudgetScenarioId'),
+			localStorage.getItem('selectedTerm'));
 	});
 
 };
