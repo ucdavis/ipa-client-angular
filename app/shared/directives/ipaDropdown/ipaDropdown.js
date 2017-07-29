44b16606fdbc6459c628f3af38216486103e818f
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-07-28 21:33:50 -0700

    Add generic dropdown directive

---
+++ app/shared/directives/ipaDropdown/ipaDropdown.js
@@ -0,0 +1,60 @@
+sharedApp.directive('ipaDropdown', function() {
+	return {
+		restrict: 'E', // Use this via an element selector <ipa-modal></ipa-modal>
+		templateUrl: 'ipaDropdown.html', // directive html found here:
+		scope: {
+			items: '<', // Each item is expected to have an id, description (display value), and selected (bool flag).
+			headerText: '<',
+			buttonText: '<',
+			searchable: '<',
+			selectItem: '&'
+		},
+		replace: true, // Replace with the template below
+		link: function(scope, element, attrs, iAttr) {
+			/* Example Usage:
+			<ipa-dropdown
+				expanded="expanded"
+				header-text="headerText"
+				button-text="buttonText"
+				items="view.state.people"
+				select-item="testSelect()">
+			</ipa-dropdown>
+			*/
+
+			// Validate passed methods
+			scope.isSelectItemSupplied = (angular.isUndefined(scope.selectItem) === false);
+
+			if (scope.isSelectItemSupplied == false) {
+				throw {
+					message:	"ipaDropdown: Required method selectItem was not supplied."
+				};
+			}
+
+			// Default values
+			scope.expanded = false;
+			scope.searchable = false;
+
+			scope.attrStyles = {
+				dropdownWidth: { width: 300 }
+			};
+
+			// Load attributes
+			if (attrs.dropdownWidth) {
+				scope.attrStyles.dropdownWidth;
+			}
+
+			if (attrs.searchable) {
+				scope.searchable = attrs.searchable;
+			}
+
+			// Methods
+			scope.toggleDropdown = function() {
+				scope.expanded = !scope.expanded;
+			};
+
+			scope.closeDropdown = function() {
+				scope.expanded = false;
+			};
+		}
+	};
+});
