b465b42c53c15ee30f0a2928cd70650981989970
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-08-28 21:21:32 -0700

    BudgetView: Add line item modal needs validation #1179

--- app/budget/directives/modals/addLineItem/addLineItem.js
+++ app/budget/directives/modals/addLineItem/addLineItem.js
@@ -10,13 +10,36 @@ budgetApp.directive("addLineItem", this.addLineItem = function ($rootScope, budg
 			scope.newLineItem = {};
 			scope.newLineItem.categoryDescription = "Select a category";
 
+			// Validates the form and builds a custom message for the user
+			scope.isFormValid = function() {
+				scope.formValidationErrorMessage = "";
+				if (!(scope.newLineItem.lineItemCategoryId) || scope.newLineItem.lineItemCategoryId == 0) {
+					scope.formValidationErrorMessage += " You must select a category.";
+				}
+
+				if (!(scope.newLineItem.amount) || scope.newLineItem.amount.length < 1) {
+					scope.formValidationErrorMessage += " You must specify an amount.";
+				}
+
+				if (!(scope.newLineItem.description) || scope.newLineItem.description.length < 1) {
+					scope.formValidationErrorMessage += " You must add a description.";
+				}
+
+				if (scope.formValidationErrorMessage.length > 0) {
+					return false;
+				}
+
+				return true;
+			};
+
+
 			scope.selectLineItemCategory = function(category) {
 				scope.newLineItem.lineItemCategoryId = category.id;
 				scope.newLineItem.categoryDescription = category.description;
 			};
+
 			scope.submitLineItemForm = function () {
 				scope.newLineItem.budgetScenarioId = scope.state.selectedBudgetScenario.id;
-
 				budgetActions.createLineItem(scope.newLineItem, scope.state.selectedBudgetScenario.id);
 			};
 		} // end link
