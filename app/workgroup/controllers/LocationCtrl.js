e4749ef096c4a69805481ad8b6d8edd8590ce482
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-09-13 11:12:21 -0700

    WorkgroupsView: Properly validate location description to avoid duplicates #1201

--- app/workgroup/controllers/LocationCtrl.js
+++ app/workgroup/controllers/LocationCtrl.js
@@ -7,6 +7,8 @@
  */
 workgroupApp.controller('LocationCtrl', ['$scope', 'workgroupActionCreators',
 		this.LocationCtrl = function ($scope, workgroupActionCreators) {
+			$scope.locationValidationError = "";
+
 			$scope.addLocation = function () {
 				workgroupActionCreators.addLocation($scope.workgroupId, $scope.view.state.locations.newLocation);
 			};
@@ -17,5 +19,32 @@ workgroupApp.controller('LocationCtrl', ['$scope', 'workgroupActionCreators',
 
 			$scope.updateLocation = function (location) {
 				workgroupActionCreators.updateLocation($scope.workgroupId, location);
+			};
+
+			$scope.isLocationFormValid = function () {
+				isFormValid = true;
+
+				// Dont attempt validation before page is initialized
+				if (!$scope.view.state) {
+					return false;
+				}
+
+				// Ensure name has been provided
+				if (!$scope.view.state.locations.newLocation.description || $scope.view.state.locations.newLocation.description.length == 0) {
+					$scope.locationValidationError = "You must provide a name for the new location";
+					isFormValid = false;
+				}
+
+				// Ensure name is unique within department
+				$scope.view.state.locations.ids.forEach(function(locationId) {
+					var location = $scope.view.state.locations.list[locationId];
+
+					if (location.description == $scope.view.state.locations.newLocation.description) {
+						$scope.locationValidationError = "You have already used this location name";
+						isFormValid = false;
+					}
+				});
+
+				return isFormValid;
 			};
 	}]);
