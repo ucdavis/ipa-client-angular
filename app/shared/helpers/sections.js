7c3d43c8765e640e64c61dd80f24e2dab27ae5ef
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-10-03 06:06:47 -0700

    SchedulingView: Add section button should display sequencePattern #853

--- app/shared/helpers/sections.js
+++ app/shared/helpers/sections.js
@@ -12,3 +12,35 @@ firstSequencePattern = function (course) {
 	// Numeric
 	return course.sequencePattern;
 };
+
+nextSequenceNumber = function (course, sectionGroup, sections) {
+	if (!course) {
+		return null;
+	}
+
+	// Numeric
+	if (course.isSeries() === false) {
+		return course.sequencePattern;
+	}
+	// Letter based
+	var lastSequenceNumber = null;
+	var newNumber = null;
+
+	if (sections.length > 0) {
+		sections.forEach(function(section) {
+			if (!lastSequenceNumber || section.sequenceNumber > lastSequenceNumber) {
+				lastSequenceNumber = section.sequenceNumber;
+			}
+		});
+
+		newNumber = parseInt(lastSequenceNumber.slice(-2)) + 1;
+
+		if (newNumber < 10) {
+			newNumber = "0" + newNumber;
+		}
+	} else {
+		newNumber = "01";
+	}
+
+	return course.sequencePattern + newNumber;
+};
