dff87c2bc3600ceef7dc32e6b924c0becd35922d
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-11-14 09:22:42 -0800

    SummaryView: Move other summary pages into separate directives #1307

---
+++ app/summary/directives/supportSummary/supportSummary.js
@@ -0,0 +1,10 @@
+summaryApp.directive("supportSummary", this.supportSummary = function () {
+	return {
+		restrict: 'E',
+		templateUrl: 'supportSummary.html',
+		replace: true,
+		link: function (scope, element, attrs) {
+			// Do nothing
+		}
+	};
+});
---
+++ app/summary/directives/workgroupSummary/workgroupSummary.js
@@ -0,0 +1,10 @@
+summaryApp.directive("workgroupSummary", this.workgroupSummary = function () {
+	return {
+		restrict: 'E',
+		templateUrl: 'workgroupSummary.html',
+		replace: true,
+		link: function (scope, element, attrs) {
+			// Do nothing
+		}
+	};
+});
