8cca7a45864f4b1c1858f450af48649839f4925f
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-11-27 09:32:34 -0800

    StudentSupportCallForm: Create new directive structure #1450

---
+++ app/instructionalSupport/studentSupportCallForm/directives/studentAvailabilities/crnAvailable/crnAvailable.js
@@ -0,0 +1,22 @@
+summaryApp.directive("crnAvailable", this.crnAvailable = function () {
+	return {
+		restrict: 'E',
+		templateUrl: 'crnAvailable.html',
+		replace: true,
+		scope: {
+			state: '<'
+		},
+		link: function (scope, element, attrs) {
+			scope.props = {};
+			scope.mapStateToProps(scope.state);
+
+			$rootScope.$on('supportStaffFormStateChanged', function (event, data) {
+				scope.mapStateToProps(data);
+			});
+
+			scope.mapStateToProps = function(state) {
+				return state;
+			};
+		}
+	};
+});
---
+++ app/instructionalSupport/studentSupportCallForm/directives/studentFormReview/studentFormReview.js
@@ -0,0 +1,22 @@
+summaryApp.directive("studentFormReview", this.studentFormReview = function () {
+	return {
+		restrict: 'E',
+		templateUrl: 'studentFormReview.html',
+		replace: true,
+		scope: {
+			state: '<'
+		},
+		link: function (scope, element, attrs) {
+			scope.props = {};
+			scope.mapStateToProps(scope.state);
+
+			$rootScope.$on('supportStaffFormStateChanged', function (event, data) {
+				scope.mapStateToProps(data);
+			});
+
+			scope.mapStateToProps = function(state) {
+				return state;
+			};
+		}
+	};
+});
