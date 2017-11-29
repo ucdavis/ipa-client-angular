8bdcdc8bba81f89a71d551a4a84c0ed2b7cc48f7
Christopher Thielen <cmthielen@ucdavis.edu>
2017-11-29 11:43:06 -0800

    Remove Konami easter egg

--- vendor/js/konami.js
+++
@@ -1,6 +0,0 @@
-var easter_egg = new Konami(function () {
-	var KICKASSVERSION = '2.0';
-	var s = document.createElement('script');
-	s.type = 'text/javascript'; document.body.appendChild(s);
-	s.src = '//hi.kickassapp.com/kickass.js';
-});
