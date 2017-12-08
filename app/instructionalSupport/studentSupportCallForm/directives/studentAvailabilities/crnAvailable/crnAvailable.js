3c67f02c4d7da41abeea621172dd1e45f5f5b780
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-12-08 11:12:03 -0800

    StudentSupportCallForm: Implementing crn search UI #1450

--- app/instructionalSupport/studentSupportCallForm/directives/studentAvailabilities/crnAvailable/crnAvailable.js
+++ app/instructionalSupport/studentSupportCallForm/directives/studentAvailabilities/crnAvailable/crnAvailable.js
@@ -1,9 +1,15 @@
-instructionalSupportApp.directive("crnAvailable", this.crnAvailable = function () {
+instructionalSupportApp.directive("crnAvailable", this.crnAvailable = function (studentActions) {
 	return {
 		restrict: 'E',
 		templateUrl: 'crnAvailable.html',
 		replace: true,
+		scope: {
+			state: '<'
+		},
 		link: function (scope, element, attrs) {
+			scope.calculateTimesForCrn = function(crn) {
+				studentActions.calculateTimesForCrn(crn, scope.state.courses, scope.state.sectionGroups, scope.state.sections, scope.state.activities);
+			};
 			// Intentionally empty
 		}
 	};
