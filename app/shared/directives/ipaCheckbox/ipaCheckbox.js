58c0e32310da5d6f381d14d4ae62ef7257ac5392
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-12-25 03:54:52 -0800

    IpaCheckbox: Add support for disabled checkboxes

--- app/shared/directives/ipaCheckbox/ipaCheckbox.js
+++ app/shared/directives/ipaCheckbox/ipaCheckbox.js
@@ -1,20 +1,24 @@
+/* Example Usage:
+<ipa-checkbox
+	is-checked="supportCall.commentsRequired"
+	is-disabled="true"
+	click-action="toggleThing()">
+</ipa-checkbox>
+*/
 sharedApp.directive("ipaCheckbox", this.ipaCheckbox = function() {
 	return {
 		restrict: 'E',
 		templateUrl: 'ipaCheckbox.html', // directive html found here:
 		scope: {
 			isChecked: '=',
-			clickAction: '&'
+			clickAction: '&?',
+			isDisabled: '<?'
 		},
 		replace: true, // Replace with the template below
 		link: function(scope, element, attrs) {
-			/* Example Usage:
-			<ipa-checkbox
-				is-checked="supportCall.commentsRequired"
-				click=action="toggleThing()">
-			</ipa-checkbox>
-			*/
-
+			scope.onClick = function() {
+				scope.clickAction();
+			};
 		}
 	};
 });
