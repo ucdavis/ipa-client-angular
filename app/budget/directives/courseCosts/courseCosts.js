813269b8667c4ed43550023cd0c542a77e3812ba
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-01-16 16:32:52 -0800

    BudgetView: Refactor courseCosts to use table based design with new layout. Refactored out courseCostRows and cleaned up remaining non-BEM css

--- app/budget/directives/courseCosts/courseCosts.js
+++ app/budget/directives/courseCosts/courseCosts.js
@@ -9,12 +9,16 @@ budgetApp.directive("courseCosts", this.courseCosts = function ($rootScope, budg
 			termNav: '<'
 		},
 		link: function (scope, element, attrs) {
-			scope.toggleCourseCostsSection = function() {
-				budgetActions.toggleCourseCostsSection();
-			};
-
 			scope.setActiveTerm = function(activeTermTab) {
 				budgetActions.selectTerm(activeTermTab);
+			};
+
+			scope.updateSectionGroupCost = function(sectionGroupCost) {
+				budgetActions.updateSectionGroupCost(sectionGroupCost);
+			};
+
+			scope.openCourseComments = function(course) {
+				budgetActions.openAddCourseCommentsModal(course);
 			};
 		} // end link
 	};
