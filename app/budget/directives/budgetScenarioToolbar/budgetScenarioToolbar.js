b6a0886b6caec2a50ba345e82fc2fe22903426cc
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-01-18 14:51:29 -0800

    BudgetView: Adding validation calculations to scenario name edit

--- app/budget/directives/budgetScenarioToolbar/budgetScenarioToolbar.js
+++ app/budget/directives/budgetScenarioToolbar/budgetScenarioToolbar.js
@@ -9,6 +9,7 @@ sharedApp.directive('budgetScenarioToolbar', function($window, $location, $route
 		link: function (scope, element, attrs) {
 			scope.displayScenarioRenameUI = false;
 			scope.newScenarioName = angular.copy(scope.state.selectedBudgetScenario.name);
+			scope.isNewScenarioNameValid = true;
 
 			scope.openSupportCostModal = function() {
 				budgetActions.toggleSupportCostModal();
@@ -27,6 +28,17 @@ sharedApp.directive('budgetScenarioToolbar', function($window, $location, $route
 			scope.saveNewScenarioName = function() {
 				scope.state.selectedBudgetScenario.name = angular.copy(scope.newScenarioName);
 				budgetActions.updateBudgetScenario(scope.state.selectedBudgetScenario);
+				scope.displayScenarioRenameUI = false;
+			};
+
+			scope.scenarioNameIsValid = function () {
+				var isNamePresent = (scope.newScenarioName.length > 0);
+
+				var isNameInUse = scope.state.budgetScenarios.any(function(scenario) {
+					return (scenario.name == scope.newScenarioName);
+				});
+
+				scope.isNewScenarioNameValid = isNamePresent && isNameInUse == false;
 			};
 		} // End Link
 	};
