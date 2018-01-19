ba4481e19587ec715094eacee98479e7ec51d3ba
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-01-18 16:23:10 -0800

    IpaInput: Properly update angular lifecycle on callback, and avoid double callback

--- app/shared/directives/ipaInput/ipaInput.js
+++ app/shared/directives/ipaInput/ipaInput.js
@@ -1,4 +1,4 @@
-sharedApp.directive("ipaInput", this.ipaInput = function () {
+sharedApp.directive("ipaInput", this.ipaInput = function ($timeout) {
 	return {
 		restrict: 'E',
 		templateUrl: 'ipaInput.html',
@@ -7,16 +7,20 @@ sharedApp.directive("ipaInput", this.ipaInput = function () {
 			onUpdate: '&?', // If set, this callback function will be triggered 500ms after changes stop
 			value: '=',
 			readOnly: '=?',
-			placeHolder: '<?'
+			placeHolder: '<?',
+			updateDelay: '<?'
 		},
 		link: function(scope, element, attrs) {
 			element.unbind("keydown keyup");
 
-			element.bind("keydown keyup", function (event) {
+			element.bind("keyup", function (event) {
 				if (angular.isUndefined(scope.onUpdate)) { return; }
 
-				clearTimeout(scope.timeout);
-				scope.timeout = setTimeout(scope.onUpdate, 500, null, true);
+				scope.delay = scope.onUpdate || 500;
+
+				$timeout.cancel(scope.timeOut);
+				
+				scope.timeOut = $timeout(scope.onUpdate, scope.delay);
 			});
 		}
 	};
