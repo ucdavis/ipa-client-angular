6be03e06a05c8faa5092cb30195d4fd16886595e
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-01-22 10:55:51 -0800

    BudgetView: Fix line item deletion from dropdown

--- app/budget/directives/lineItems/lineItemDropdown/lineItemDropdown.js
+++ app/budget/directives/lineItems/lineItemDropdown/lineItemDropdown.js
@@ -10,6 +10,13 @@ budgetApp.directive("lineItemDropdown", this.lineItemDropdown = function ($rootS
 			scope.openEditLineItemModal = function(lineItem) {
 				budgetActions.openAddLineItemModal(lineItem);
 			};
+
+			scope.deleteLineItem = function(lineItem) {
+				budgetActions.deleteLineItem(lineItem);
+
+				// Ensure bootstrap dropdown closes properly when confirming deleting line item
+				$(this).closest(".line-item-edit-dropdown").prev().dropdown("toggle");
+			};
 		} // end link
 	};
 });
