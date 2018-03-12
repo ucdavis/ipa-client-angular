9a5b01a5b1efcafcaee047e9df142829ed41562c
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-03-12 16:48:32 -0700

    SchedulingView: Fix section selection color changing behavior with tags

--- app/scheduling/directives/termCalendar.js
+++ app/scheduling/directives/termCalendar.js
@@ -107,43 +107,6 @@ schedulingApp.directive("termCalendar", this.termCalendar = function ($rootScope
 				});
 			};
 
-			// Supply a color and amount to shift the color (out of 255)
-			// Example to lighten: lightenOrDarkenColor("#F06D06", 20);
-			// Example to darken: lightenOrDarkenColor("#F06D06", -20);
-			var lightenOrDarkenColor = function(col, amt) {
-				var usePound = false;
-
-				if (col[0] == "#") {
-					col = col.slice(1);
-					usePound = true;
-				}
-
-				var num = parseInt(col,16);
-				var r = (num >> 16) + amt;
-
-				if (r > 255) {
-					r = 255;
-				} else if (r < 0) {
-					r = 0;
-				}
-
-				var b = ((num >> 8) & 0x00FF) + amt;
-
-				if (b > 255) {
-					b = 255;
-				} else if (b < 0) { b = 0; }
-
-				var g = (num & 0x0000FF) + amt;
-
-				if (g > 255) {
-					g = 255;
-				} else if (g < 0) {
-					g = 0;
-				}
-
-				return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
-			};
-
 			var getActivities = function () {
 				// Each of these If blocks will add to a 'events array'
 				// The event making function will color them appropriately
@@ -314,15 +277,9 @@ schedulingApp.directive("termCalendar", this.termCalendar = function ($rootScope
 			var styleCalendarEvents = function (calendarActivities, backgroundColor, borderColor, textColor, tagColor) {
 				calendarActivities.forEach(function (event) {
 					if (scope.view.state.uiState.selectedActivityId === event.activityId) {
-						if (tagColor) {
-							event.color = lightenOrDarkenColor(tagColor, selectedActivityTaggedColorShift);
-							event.borderColor = lightenOrDarkenColor(tagColor, selectedActivityTaggedColorShift);
-							event.textColor = textColor;
-						} else {
 							event.color = highlightedEventBackgroundColor;
 							event.borderColor = highlightedEventBorderColor;
 							event.textColor = highlightedEventTextColor;
-						}
 					} else {
 						event.color = angular.copy(backgroundColor);
 						event.borderColor = angular.copy(borderColor);
