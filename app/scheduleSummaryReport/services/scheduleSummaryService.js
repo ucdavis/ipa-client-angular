80ab0f455a56446b0a9f300e3ffbc1a129122181
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-04-09 14:38:20 -0700

    Refactoring reportServices for angular upgrade

--- app/scheduleSummaryReport/services/scheduleSummaryService.js
+++ app/scheduleSummaryReport/services/scheduleSummaryService.js
@@ -6,34 +6,13 @@
  * Service in the reportApp.
  * reportApp specific api calls.
  */
-scheduleSummaryReportApp.factory("scheduleSummaryReportService", this.scheduleSummaryReportService = function ($http, $q, $window) {
+scheduleSummaryReportApp.factory("scheduleSummaryReportService", this.scheduleSummaryReportService = function (apiService) {
 	return {
 		getInitialState: function (workgroupId, year, termCode) {
-			var deferred = $q.defer();
-
-			$http.get(serverRoot + "/api/scheduleSummaryReportView/workgroups/" + workgroupId + "/years/" + year + "/terms/" + termCode, { withCredentials: true })
-				.success(function (payload) {
-					deferred.resolve(payload);
-				})
-				.error(function () {
-					deferred.reject();
-				});
-
-			return deferred.promise;
+			return apiService.get("/api/scheduleSummaryReportView/workgroups/" + workgroupId + "/years/" + year + "/terms/" + termCode);
 		},
 		downloadSchedule: function (workgroupId, year, shortTermCode) {
-			var deferred = $q.defer();
-
-			$http.get(serverRoot + "/api/scheduleSummaryReportView/workgroups/" + workgroupId + "/years/" + year + "/terms/" + shortTermCode + "/generateExcel", { withCredentials: true })
-				.success(function (payload) {
-					$window.location.href = payload.redirect;
-					deferred.resolve(payload);
-				})
-				.error(function () {
-					deferred.reject();
-				});
-
-			return deferred.promise;
+			return apiService.get("/api/scheduleSummaryReportView/workgroups/" + workgroupId + "/years/" + year + "/terms/" + shortTermCode + "/generateExcel");
 		}
 	};
 });
