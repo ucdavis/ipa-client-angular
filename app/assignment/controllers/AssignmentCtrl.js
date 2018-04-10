5cab6113a44cb101eedc13561d923b4aa150d0ab
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-04-09 17:02:58 -0700

    Adding explicit close modal handling to uibBootstrap modals

--- app/assignment/controllers/AssignmentCtrl.js
+++ app/assignment/controllers/AssignmentCtrl.js
@@ -78,54 +78,6 @@ assignmentApp.controller('AssignmentCtrl', ['$scope', '$rootScope', '$window', '
 				assignmentActionCreators.updateTagFilters(tagFilters);
 			};
 
-			// Launches TeachingCall Config modal and controller
-			$scope.openTeachingCallConfig = function() {
-				modalInstance = $uibModal.open({
-					templateUrl: 'ModalTeachingCallConfig.html',
-					controller: ModalTeachingCallConfigCtrl,
-					size: 'lg',
-					resolve: {
-						scheduleYear: function () {
-							return $scope.year;
-						},
-						workgroupId: function () {
-							return $scope.workgroupId;
-						},
-						viewState: function () {
-							return $scope.view.state;
-						},
-						allTerms: function () {
-							return assignmentService.allTerms();
-						}
-					}
-				});
-
-				modalInstance.result.then(function (teachingCallConfig) {
-					$scope.startTeachingCall($scope.workgroupId, $scope.year, teachingCallConfig);
-				},
-				function () {
-					// Modal closed
-				});
-			};
-
-			// Triggered on TeachingCall Config submission
-			$scope.startTeachingCall = function(workgroupId, year, teachingCallConfig) {
-				teachingCallConfig.termsBlob = "";
-				var allTerms = ['05','06','07','08','09','10','01','02','03'];
-
-				for (var i = 0; i < allTerms.length; i++) {
-					if (teachingCallConfig.activeTerms[allTerms[i]] === true) {
-						teachingCallConfig.termsBlob += "1";
-					} else {
-						teachingCallConfig.termsBlob += "0";
-					}
-				}
-
-				delete teachingCallConfig.activeTerms;
-
-				assignmentActionCreators.createTeachingCall(workgroupId, year, teachingCallConfig);
-			};
-
 			// Launched from the instructorTable directive UI handler
 			$scope.openCommentModal = function(instructorId) {
 				var instructor = $scope.view.state.instructors.list[instructorId];
@@ -209,6 +161,13 @@ assignmentApp.controller('AssignmentCtrl', ['$scope', '$rootScope', '$window', '
 							return instructor;
 						}
 					}
+				});
+
+				modalInstance.result.then(function () {
+					// This modal does not 'submit' in a traditional sense.
+				},
+				function () {
+					// Modal closed
 				});
 			};
 
