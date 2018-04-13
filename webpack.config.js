8b72ea49ba3a7b9666680344652641472393801e
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-04-13 11:45:19 -0700

    Fixing bugs in rewrite rules

--- webpack.config.js
+++ webpack.config.js
@@ -240,25 +240,24 @@ module.exports = {
     inline: true,
     historyApiFallback: {
       index: 'index.html',
-      historyApiFallback: {
-        disableDotRule: true,
-        rewrites: [
-          { from: /^\/admin/, to: 'admin.html' },
-          { from: /^\/budget/, to: 'budget.html' },
-          { from: /^\/workgroups/, to: 'workgroups.html' },
-          { from: /^\/summary/, to: 'summary.html' },
-          { from: /^\/courses/, to: 'courses.html' },
-          { from: /^\/assignments/, to: 'assignments.html' },
-          { from: /^\/instructionalSupport/, to: 'instructionalSupport.html' },
-          { from: /^\/teachingCalls/, to: 'teachingCalls.html' },
-          { from: /^\/supportCalls/, to: 'supportCalls.html' },
-          { from: /^\/supportAssignments/, to: 'supportAssignments.html' },
-          { from: /^\/scheduling/, to: 'scheduling.html' },
-          { from: /^\/registrarReconciliationReport/, to: 'registrarReconciliationReport.html' },
-          { from: /^\/scheduleSummaryReport/, to: 'scheduleSummaryReport.html' },
-          { from: /^\/teachingCallResponseReport/, to: 'teachingCallResponseReport.html' }
-        ]
-      }
+      disableDotRule: false,
+      rewrites: [
+        { from: /^\/admin.*/, to: '/admin.html' },
+        { from: /^\/budget.*/, to: '/budget.html' },
+        { from: /^\/workgroups.*/, to: '/workgroups.html' },
+        { from: /^\/summary.*/, to: '/summary.html' },
+        { from: /^\/courses.*/, to: '/courses.html' },
+        { from: /^\/assignments.*/, to: '/assignments.html' },
+        { from: /^\/instructionalSupport.*/, to: '/instructionalSupport.html' },
+        { from: /^\/teachingCalls.*/, to: '/teachingCalls.html' },
+        { from: /^\/supportCalls.*/, to: '/supportCalls.html' },
+        { from: /^\/supportAssignments.*/, to: '/supportAssignments.html' },
+        { from: /^\/scheduling.*/, to: '/scheduling.html' },
+        { from: /^\/registrarReconciliationReport.*/, to: '/registrarReconciliationReport.html' },
+        { from: /^\/scheduleSummaryReport.*/, to: '/scheduleSummaryReport.html' },
+        { from: /^\/teachingCallResponseReport.*/, to: '/teachingCallResponseReport.html' },
+        { from: /./, to: '/not-found.html' }
+      ]
     }
   }
 };
