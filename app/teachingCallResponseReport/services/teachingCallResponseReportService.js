ca2117db6d9b478a8c31be826a02a74baf518f73
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-04-24 13:18:53 -0700

    Refactor teachingCallResponseReportService into es6

--- app/teachingCallResponseReport/services/teachingCallResponseReportService.js
+++ app/teachingCallResponseReport/services/teachingCallResponseReportService.js
@@ -6,22 +6,33 @@
  * Service in the teachingCallResponseReportApp.
  * teachingCallResponseReportApp specific api calls.
  */
-teachingCallResponseReportApp.factory("teachingCallResponseReportService", this.teachingCallResponseReportService = function ($http, $q, $window, apiService) {
-	return {
-		getInitialState: function (workgroupId, year) {
-			return apiService.get("/api/teachingCallResponseReportView/workgroups/" + workgroupId + "/years/" + year);
-		},
-		download: function (workgroupId, year) {
-			$http.get(serverRoot + "/api/teachingCallResponseReportView/workgroups/" + workgroupId + "/years/" + year + "/generateExcel", { withCredentials: true })
-			.then(function(payload) {
-				$window.location.href = payload.redirect;
-				deferred.resolve(payload);
-			},
-			function() {
-				deferred.reject();
-			});
+class TeachingCallResponseReportService {
+	constructor($http, $q, $window, apiService) {
+		this.$http = $http;
+		this.$q = $1q;
+		this.$window = $window;
+		this.apiService = apiService;
 
-			return deferred.promise;
-		}
-	};
-});
+		return {
+			getInitialState: function (workgroupId, year) {
+				return apiService.get("/api/teachingCallResponseReportView/workgroups/" + workgroupId + "/years/" + year);
+			},
+			download: function (workgroupId, year) {
+				$http.get(serverRoot + "/api/teachingCallResponseReportView/workgroups/" + workgroupId + "/years/" + year + "/generateExcel", { withCredentials: true })
+				.then(function(payload) {
+					$window.location.href = payload.redirect;
+					deferred.resolve(payload);
+				},
+				function() {
+					deferred.reject();
+				});
+	
+				return deferred.promise;
+			}
+		};
+	}
+}
+
+TeachingCallResponseReportService.$inject = ['$http', '$q', '$window', 'apiService'];
+
+export default TeachingCallResponseReportService;
