e5d1d2e6d2d6751485a67ad3adc3ada69658b7cc
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-04-24 13:13:35 -0700

    Refactor teachingCallResponseReportActionCreators into es6

--- app/teachingCallResponseReport/services/teachingCallResponseReportActionCreators.js
+++ app/teachingCallResponseReport/services/teachingCallResponseReportActionCreators.js
@@ -1,16 +1,27 @@
-teachingCallResponseReportApp.service('teachingCallResponseReportActionCreators', function (teachingCallResponseReportStateService, teachingCallResponseReportService, $rootScope) {
-	return {
-		getInitialState: function (workgroupId, year, termCode) {
-			teachingCallResponseReportService.getInitialState(workgroupId, year).then(function (payload) {
-				var action = {
-					type: INIT_STATE,
-					payload: payload,
-					year: year
-				};
-				teachingCallResponseReportStateService.reduce(action);
-			}, function (err) {
-				$rootScope.$emit('toast', { message: "Could not load teaching call response report initial state.", type: "ERROR" });
-			});
-		}
+class TeachingCallResponseReportActionCreators {
+	constructor(teachingCallResponseReportStateService, teachingCallResponseReportService, $rootScope, ActionTypes) {
+		this.teachingCallResponseReportStateService = teachingCallResponseReportStateService;
+		this.teachingCallResponseReportService = teachingCallResponseReportService;
+		this.$rootScope = $rootScope;
+		this.ActionTypes = ActionTypes;
+
+		return {
+			getInitialState: function (workgroupId, year, termCode) {
+				teachingCallResponseReportService.getInitialState(workgroupId, year).then(function (payload) {
+					var action = {
+						type: INIT_STATE,
+						payload: payload,
+						year: year
+					};
+					teachingCallResponseReportStateService.reduce(action);
+				}, function (err) {
+					$rootScope.$emit('toast', { message: "Could not load teaching call response report initial state.", type: "ERROR" });
+				});
+			}
+		};	
 	};
-});
+}
+
+TeachingCallResponseReportActionCreators.$inject = ['TeachingCallResponseReportStateService', 'TeachingCallResponseReportService', '$rootScope', 'ActionTypes'];
+
+export default TeachingCallResponseReportActionCreators;
