47c5e66e4400f3c64970613103116d13fd8dfe93
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-05-25 13:50:21 -0700

    Removing deprecated code

--- app/budget/directives/budgetScenarioToolbar/budgetScenarioToolbar.js
+++ app/budget/directives/budgetScenarioToolbar/budgetScenarioToolbar.js
@@ -57,16 +57,6 @@ let budgetScenarioToolbar = function($window, $location, $routeParams, $rootScop
 				}
 
 				scope.isNewScenarioNameValid = isNamePresent && (isNameInUse == false);
-
-
-				scope.selectBudgetScenarioTerm = function(term) {
-					debugger;
-					var index = parseInt(term) - 1;
-					var newValue = scope.state.selectedBudgetScenario.activeTermsBlob[index] == "1" ? "0" : "1";
-					scope.state.selectedBudgetScenario.activeTermsBlob = setCharAt(scope.state.selectedBudgetScenario.activeTermsBlob, index, newValue);
-	
-					BudgetActions.updateBudgetScenario(scope.state.selectedBudgetScenario);
-				};
 			};
 
 			scope.selectBudgetScenarioTerm = function(term) {
