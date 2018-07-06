b917bf86f8ac7bae84dd65fa7987dc3d4354b396
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-07-06 10:30:53 -0700

    Initial scaffolding of manually created Angular app

---
+++ app/newScheduling/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-main',
+  template: '<h1>Hello, {{name}}</h1>'
+})
+export class AppComponent {
+  name = 'Angular';
+}
---
+++ app/newScheduling/scheduling.module.ts
@@ -0,0 +1,10 @@
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
