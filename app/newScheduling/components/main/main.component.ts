972600899832f9b31901b88d0f431d0fa6b4e938
Jarold Wong <jaroldw@gmail.com>
2018-07-26 10:17:04 -0700

    Example implementation of  Material tooltip and buttons

--- app/newScheduling/components/main/main.component.ts
+++ app/newScheduling/components/main/main.component.ts
@@ -14,6 +14,7 @@ import * as toastr from 'toastr';
   templateUrl: './main.component.html'
 })
 export class MainComponent {
+  tooltip: string = 'Taco';
   scheduling: Observable<SchedulingState>;
   schedulingName: String;
   schedulingShow: boolean;
