b670286bf4467bd5581289be3bcbf9aa62940e45
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-08-02 10:08:22 -0700

    Ensure students get a link to a support assignments that is open for review

--- app/summary/directives/supportSummary/supportSummary.js
+++ app/summary/directives/supportSummary/supportSummary.js
@@ -1,10 +1,18 @@
-let supportSummary = function () {
+let supportSummary = function (TermService) {
 	return {
 		restrict: 'E',
 		template: require('./supportSummary.html'),
 		replace: true,
 		link: function (scope, element, attrs) {
-			// Do nothing
+			scope.orderedTerms = [5, 6, 7, 8, 9, 10, 1, 2, 3];
+
+			scope.termDescription = function(term) {
+				return TermService.getShortTermName(term);
+			};
+
+			scope.isReviewOpenForTerm = function(term, reviewBlob) {
+				return reviewBlob[term - 1] == "1";
+			};
 		}
 	};
 };
