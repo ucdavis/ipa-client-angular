2c39fea395b68bad2f0b93c4fbfd2a90817ece13
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-08-06 09:55:41 -0700

    Fixing small styling issues in header/nav and removing the original variants

--- app/core/components/ipa-nav-variant-two/nav-accordion.model.ts
+++
@@ -1,8 +0,0 @@
-import { INavLink } from './nav-link.model';
-
-export interface INavAccordion {
-  description: string;
-  key: string;
-  icon: string;
-  links: INavLink[];
-}
--- app/core/components/ipa-nav-variant-two/nav-link.model.ts
+++
@@ -1,4 +0,0 @@
-export interface INavLink {
-  description: string;
-  key: string;
-}
