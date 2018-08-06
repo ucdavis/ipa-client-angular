baaca0f35eb447df0c4dd8ebb7591aeafd1c2f78
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-08-06 09:55:51 -0700

    removing unused import

--- app/core/resolvers/section-group-resolver.service.ts
+++ app/core/resolvers/section-group-resolver.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router, Resolve, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map, take } from 'rxjs/operators';
 
 import { ApiService }  from '@core/api/api.service';
 
