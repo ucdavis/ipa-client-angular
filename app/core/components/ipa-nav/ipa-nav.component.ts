e4c447b4e864fb481d2fb49d87a6d882aee45763
Jarold Wong <jaroldw@gmail.com>
2018-08-08 09:56:00 -0700

    Read active page path from router

--- app/core/components/ipa-nav/ipa-nav.component.ts
+++ app/core/components/ipa-nav/ipa-nav.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'ipa-nav',
   templateUrl: './ipa-nav.component.html',
   styleUrls: ['./ipa-nav.component.css']
 })
-export class IpaNav {
+export class IpaNav implements OnInit {
   isNavExpanded: boolean = true;
-  activePage: string = 'schedule';
+  activePage: string;
 
-  constructor() {}
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.activePage = this.route.snapshot.url[0].path;
+  }
 
   toggleSideNav() {
     this.isNavExpanded = !this.isNavExpanded;
