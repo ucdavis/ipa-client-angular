efb882951ea89cad0c09f3a0e6cf81a18e3ddba2
Jarold Wong <jaroldw@gmail.com>
2018-08-08 11:20:16 -0700

    Implement year selector functionality to update url on change

--- app/core/components/ipa-header/ipa-header.component.ts
+++ app/core/components/ipa-header/ipa-header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router, UrlSegment } from '@angular/router';
+import { SharedStateService } from '@core/shared-state/shared-state.service';
 
 @Component({
   selector: 'ipa-header',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./ipa-header.component.css']
 })
 export class IpaHeader {
-  constructor() {}
+  year: string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private sharedState: SharedStateService
+  ) {}
+
+  ngOnInit() {
+    this.year = this.route.snapshot.url[2].path;
+  }
+
+  offsetYearInUrl(offset: number) {
+    const currentYear = this.year;
+    const currentPath = this.router.url.split(currentYear);
+    this.year = (parseInt(this.year) + offset).toString();
+
+    const updatedPath = currentPath[0] + this.year + currentPath[1];
+    this.router.navigate([updatedPath]);
+  }
+
+  yearToAcademicYear() {
+    return `${this.year} - ${(parseInt(this.year) + 1).toString().slice(-2)}`;
+  }
 }
