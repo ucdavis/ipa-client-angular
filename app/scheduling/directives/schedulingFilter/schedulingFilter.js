7fb32b6508294f4b4a98ed53d7c9e0b371ff71e1
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-09-03 16:50:22 -0700

    Re-implementing filter actions

--- app/scheduling/directives/schedulingFilter/schedulingFilter.js
+++ app/scheduling/directives/schedulingFilter/schedulingFilter.js
@@ -16,6 +16,42 @@ let schedulingFilter = function (SchedulingActionCreators) {
       scope.toggleCalendarDay = function (index) {
         SchedulingActionCreators.toggleDay(index);
       };
+
+      scope.toggleTagFilter = function (tagId) {
+        var tagIndex = scope.filters.enabledTagIds.indexOf(tagId);
+
+        if (tagIndex < 0) {
+          scope.filters.enabledTagIds.push(tagId);
+        } else {
+          scope.filters.enabledTagIds.splice(tagIndex, 1);
+        }
+
+        SchedulingActionCreators.updateTagFilters(scope.filters.enabledTagIds);
+      };
+
+      scope.toggleLocationFilter = function (locationId) {
+        var locationIndex = scope.filters.enabledLocationIds.indexOf(locationId);
+
+        if (locationIndex < 0) {
+          scope.filters.enabledLocationIds.push(locationId);
+        } else {
+          scope.filters.enabledLocationIds.splice(locationIndex, 1);
+        }
+
+        SchedulingActionCreators.updateLocationFilters(scope.filters.enabledLocationIds);
+      };
+
+      scope.toggleInstructorFilter = function (instructorId) {
+        var instructorIndex = scope.filters.enabledInstructorIds.indexOf(instructorId);
+
+        if (instructorIndex < 0) {
+          scope.filters.enabledInstructorIds.push(instructorId);
+        } else {
+          scope.filters.enabledInstructorIds.splice(instructorIndex, 1);
+        }
+
+        SchedulingActionCreators.updateInstructorFilters(scope.filters.enabledInstructorIds);
+      };
     }
   };
 };
