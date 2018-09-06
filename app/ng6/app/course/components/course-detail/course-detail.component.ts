c142ef1b2159a53a669767bd8a443a1c52860028
Jarold Wong <jaroldw@gmail.com>
2018-09-06 10:33:34 -0700

    Add course detail component

---
+++ app/ng6/app/course/components/course-detail/course-detail.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-detail',
+  templateUrl: './course-detail.component.html',
+  styleUrls: ['course-detail.component.css']
+})
+export class CourseDetailComponent {}
--- app/ng6/app/course/course.module.ts
+++ app/ng6/app/course/course.module.ts
@@ -5,9 +5,10 @@ import { SharedModule } from '@shared/shared.module';
 import { CourseRoutingModule } from './course-routing.module';
 
 import { CoursePageComponent } from './components/course-page/course-page.component';
+import { CourseDetailComponent } from './components/course-detail/course-detail.component';
 
 @NgModule({
   imports: [CourseRoutingModule, SharedModule],
-  declarations: [CoursePageComponent]
+  declarations: [CoursePageComponent, CourseDetailComponent]
 })
 export class CourseModule {}
