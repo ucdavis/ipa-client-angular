10f516dca15e226b7ed7597ab8fb392207961b5f
Jarold Wong <jaroldw@gmail.com>
2018-10-25 10:47:05 -0700

    Rename variable

--- app/teachingCall/teachingCallStatus/controllers/TeachingCallStatusCtrl.js
+++ app/teachingCall/teachingCallStatus/controllers/TeachingCallStatusCtrl.js
@@ -24,11 +24,10 @@ class TeachingCallStatusCtrl {
 
 		$rootScope.$on('teachingCallStatusStateChanged', function (event, data) {
 			$scope.view.state = data;
-			console.log(data);
 
 			TeachingCallStatusService.getCourses($scope.workgroupId, $scope.year)
 				.then(function (res) {
-					res.courses.length === 0 ? $scope.view.state.isCourseTableEmpty = true : $scope.view.state.isCourseTableEmpty = false;
+					$scope.view.state.scheduleHasCourses = (res.courses.length === 0);
 				});
 		});
 
