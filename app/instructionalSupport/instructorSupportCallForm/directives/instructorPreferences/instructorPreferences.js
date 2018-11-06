c499c64cc754e976a31bc66df610895b98b8779b
Jarold Wong <jaroldw@gmail.com>
2018-11-06 10:58:21 -0800

    Implement Search/filter for all TAs list

--- app/instructionalSupport/instructorSupportCallForm/directives/instructorPreferences/instructorPreferences.js
+++ app/instructionalSupport/instructorSupportCallForm/directives/instructorPreferences/instructorPreferences.js
@@ -13,6 +13,31 @@ let instructorPreferences = function ($rootScope, InstructorFormActions) {
       supportStaffList: '<'
     },
 		link: function (scope, element, attrs) {
+      scope.filteredSupportStaff = scope.supportStaffList.sorted;
+
+      scope.filterSupportStaff = function(searchQuery) {
+        if(searchQuery.length >= 1) {
+          var options = {
+            shouldSort: true,
+            threshold: 0.3,
+            location: 0,
+            distance: 100,
+            maxPatternLength: 32,
+            minMatchCharLength: 1,
+            includeScore: false,
+            keys: [
+              "fullName"
+            ]
+          };
+        
+          var fuse = new Fuse(scope.supportStaffList.sorted, options);
+          var results = fuse.search(searchQuery);
+          scope.filteredSupportStaff = results;
+        } else {
+          scope.filteredSupportStaff = scope.supportStaffList.sorted;
+        }
+      };
+
       scope.addPreference = function(sectionGroupId, supportStaffId) {
         InstructorFormActions.addInstructorPreference(sectionGroupId, supportStaffId);
       };
