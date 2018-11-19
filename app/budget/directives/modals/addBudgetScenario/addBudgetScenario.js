c4a83096d4bee2e901c477272e6b8cd3a404fc15
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-11-19 11:46:03 -0800

    Add copy Funds checkbox

--- app/budget/directives/modals/addBudgetScenario/addBudgetScenario.js
+++ app/budget/directives/modals/addBudgetScenario/addBudgetScenario.js
@@ -1,40 +1,46 @@
 import './addBudgetScenario.css';
 
 let addBudgetScenario = function ($rootScope, BudgetActions) {
-	return {
-		restrict: 'E',
-		template: require('./addBudgetScenario.html'),
-		replace: true,
-		scope: {
-			state: '<',
-			isVisible: '='
-		},
-		link: function (scope, element, attrs) {
-			scope.newBudgetScenario = {};
-			scope.newBudgetScenario.name = "";
-			scope.newBudgetScenario.budgetScenarioId = 0;
-			scope.newBudgetScenario.description = "Schedule Data";
+  return {
+    restrict: 'E',
+    template: require('./addBudgetScenario.html'),
+    replace: true,
+    scope: {
+      state: '<',
+      isVisible: '='
+    },
+    link: function (scope, element, attrs) {
+      scope.newBudgetScenario = {};
+      scope.newBudgetScenario.name = "";
+      scope.newBudgetScenario.budgetScenarioId = 0;
+      scope.newBudgetScenario.description = "Schedule Data";
+      scope.newBudgetScenario.copyFunds = true;
 
-			scope.budgetId = attrs.budgetId;
+      scope.budgetId = attrs.budgetId;
 
-			scope.selectBudgetScenario = function(budgetScenario) {
-				if (budgetScenario == null) {
-					scope.newBudgetScenario.budgetScenarioId = 0;
-					scope.newBudgetScenario.description = "Schedule Data";
-				} else {
-					scope.newBudgetScenario.budgetScenarioId = budgetScenario.id;
-					scope.newBudgetScenario.description = budgetScenario.name;
-				}
-			};
-			scope.submitBudgetScenarioForm = function () {
-				BudgetActions.createBudgetScenario(scope.newBudgetScenario, scope.state.budget.id, scope.newBudgetScenario.budgetScenarioId);
-			};
+      scope.selectBudgetScenario = function(budgetScenario) {
+        if (budgetScenario == null) {
+          scope.newBudgetScenario.budgetScenarioId = 0;
+          scope.newBudgetScenario.description = "Schedule Data";
+        } else {
+          scope.newBudgetScenario.budgetScenarioId = budgetScenario.id;
+          scope.newBudgetScenario.description = budgetScenario.name;
+        }
+      };
 
-			scope.close = function() {
-				scope.isVisible = false;
-			};
-		} // end link
-	};
+      scope.submitBudgetScenarioForm = function () {
+        BudgetActions.createBudgetScenario(scope.newBudgetScenario, scope.state.budget.id, scope.newBudgetScenario.budgetScenarioId);
+      };
+
+      scope.toggleCopyFunds = function () {
+        scope.newBudgetScenario.copyFunds = !scope.newBudgetScenario.copyFunds;
+      };
+
+      scope.close = function() {
+        scope.isVisible = false;
+      };
+    }
+  };
 };
 
 export default addBudgetScenario;
