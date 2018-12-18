c9ef8209fd514412e6c34da5118e882d84c680f0
Mauricio Romero <mromeroval@ucdavis.edu>
2018-12-17 18:39:53 -0800

    Rewrote if/else statement back to previous version

--- app/shared/directives/ipaTermSelector/ipaTermSelector.js
+++ app/shared/directives/ipaTermSelector/ipaTermSelector.js
@@ -21,9 +21,15 @@ let ipaTermSelector = function($window, $location, $routeParams, $rootScope) {
 
 			// Generates display text for the center of the term selector
 			scope.generateDisplayText = function() {
+				if (!scope.termShortCode || scope.termShortCode.length != 2) {
+					return "Annual";
+				}
+
 				var description = scope.termShortCode.getTermDisplayName();
 				if (description && description.length > 0) {
 					return description;
+				} else {
+					return "Annual";
 				}
 			};
 
