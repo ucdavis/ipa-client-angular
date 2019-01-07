60b63de34d22c439e04be39fccaebecdda73334b
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2019-01-07 04:50:37 -0800

    Add workload summary report splash screen

--- app/workloadSummaryReport/workloadSummaryReportCtrl.js
+++ app/workloadSummaryReport/workloadSummaryReportCtrl.js
@@ -1,11 +1,12 @@
 class WorkloadSummaryReportCtrl {
-	constructor ($scope, $rootScope, $routeParams, WorkloadSummaryActions, AuthService) {
+	constructor ($scope, $rootScope, $routeParams, WorkloadSummaryActions, AuthService, validate) {
 		this.$scope = $scope;
 		this.$rootScope = $rootScope;
 		this.$routeParams = $routeParams;
 		this.WorkloadSummaryActions = WorkloadSummaryActions;
 		this.AuthService = AuthService;
 
+		$scope.noAccess = validate ? validate.noAccess : null;
 		$scope.workgroupId = $routeParams.workgroupId;
 		$scope.year = $routeParams.year;
 
@@ -14,9 +15,15 @@ class WorkloadSummaryReportCtrl {
 		$rootScope.$on('workloadSummaryStateChanged', function (event, data) {
 			$scope.view.state = data.state;
 		});
+
+		$scope.sharedState = $scope.sharedState || AuthService.getSharedState();
+
+		$rootScope.$on('sharedStateSet', function (event, data) {
+			$scope.sharedState = data;
+		});
 	}
 }
 
-WorkloadSummaryReportCtrl.$inject = ['$scope', '$rootScope', '$routeParams', 'WorkloadSummaryActions', 'AuthService'];
+WorkloadSummaryReportCtrl.$inject = ['$scope', '$rootScope', '$routeParams', 'WorkloadSummaryActions', 'AuthService', 'validate'];
 
 export default WorkloadSummaryReportCtrl;
