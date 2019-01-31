7227b1a3e22fe3bc0a0d4343d4003aaafa5169cb
mauromero <mromeroval@ucdavis.edu>
2019-01-31 14:09:24 -0800

    Added option to display component as button or input

--- app/shared/directives/ipaDropdownSelect/ipaDropdownSelect.js
+++ app/shared/directives/ipaDropdownSelect/ipaDropdownSelect.js
@@ -3,12 +3,22 @@ let ipaDropdownSelect = function() {
 		restrict: 'E',
 		template: require('./ipaDropdownSelect.html'),
 		replace: true,
+		
 		scope: {
 			items: '<', // Each item is expected to have an id, description (display value), and selected (bool flag). A rowType value = 'subheader' will display as header for another list group
 			buttonText: '<',
 			selectItem: '&',
+			showAs: '@', // Use 'button' or 'input' to show the component as such. Input is default.
 		},
-		link: function(scope){
+		link: function(scope) {
+		/* Example Usage:
+			<ipa-dropdown-select 
+				button-text="'Search Person'"
+				show-as="input"
+				items="data"
+				select-item="addItem(item)">
+			</ipa-dropdown-select>
+		*/
 
 			scope.toggleDropdown = function() {
 				scope.expanded = !scope.expanded;
@@ -26,6 +36,10 @@ let ipaDropdownSelect = function() {
 				scope.selectItem({item: item});
 				scope.closeDropdown();
 			};
+
+
+			// Default values
+			scope.showAs = "input";
 
 		}
 
