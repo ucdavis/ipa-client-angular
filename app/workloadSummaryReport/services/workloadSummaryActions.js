9427be58bda418bae82e0bdefb92312a34560309
Jarold Wong <jaroldw@gmail.com>
2019-02-04 14:32:56 -0800

    Update totals to include seats

--- app/workloadSummaryReport/services/workloadSummaryActions.js
+++ app/workloadSummaryReport/services/workloadSummaryActions.js
@@ -292,6 +292,7 @@ class WorkloadSummaryActions {
 						byInstructorTypeId: {},
 						units: 0,
 						studentCreditHours: 0,
+						seats: 0,
 						enrollment: 0,
 						previousEnrollment: 0,
 						instructorCount: 0,
@@ -324,6 +325,7 @@ class WorkloadSummaryActions {
 					calculatedView.totals.byInstructorTypeId[instructorTypeId] = calculatedView.totals.byInstructorTypeId[instructorTypeId] || {
 						units: 0,
 						studentCreditHours: 0,
+						seats: 0,
 						enrollment: 0,
 						previousEnrollment: 0,
 						assignmentCount: 0
@@ -367,13 +369,15 @@ class WorkloadSummaryActions {
 							assignment.studentCreditHours = assignment.seats * assignment.units;
 
 							calculatedView.totals.assignmentCount += 1;
-							calculatedView.totals.enrollment += assignment.enrollment;
+							calculatedView.totals.seats += assignment.seats;
+							calculatedView.totals.enrollment += assignment.actualEnrollment;
 							calculatedView.totals.previousEnrollment += assignment.previousEnrollment;
 							calculatedView.totals.units += assignment.units;
 							calculatedView.totals.studentCreditHours += assignment.studentCreditHours;
 
 							calculatedView.totals.byInstructorTypeId[instructorTypeId].assignmentCount += 1;
-							calculatedView.totals.byInstructorTypeId[instructorTypeId].enrollment += assignment.enrollment;
+							calculatedView.totals.byInstructorTypeId[instructorTypeId].seats += assignment.seats;
+							calculatedView.totals.byInstructorTypeId[instructorTypeId].enrollment += assignment.actualEnrollment;
 							calculatedView.totals.byInstructorTypeId[instructorTypeId].previousEnrollment += assignment.previousEnrollment;
 							calculatedView.totals.byInstructorTypeId[instructorTypeId].units += assignment.units;
 							calculatedView.totals.byInstructorTypeId[instructorTypeId].studentCreditHours += assignment.studentCreditHours;
