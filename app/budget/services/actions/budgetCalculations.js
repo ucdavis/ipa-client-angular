6bfa0bf6baafacba1966b9c2a967326dda75d036
Jarold Wong <jaroldw@gmail.com>
2019-02-07 10:58:47 -0800

    Calculate instructor type if missing (#1660)

--- app/budget/services/actions/budgetCalculations.js
+++ app/budget/services/actions/budgetCalculations.js
@@ -605,7 +605,7 @@ class BudgetCalculations {
 				if (!instructorTypeId && !instructor) { return replacementCosts; }
 
 				if (!instructorTypeId) {
-					instructorTypeId = instructor.instructorType.id;
+					instructorTypeId = this._calculateInstructorType(instructor.id).id;
 				}
 
 				var index = replacementCosts.instructorTypeIds.indexOf(instructorTypeId);
