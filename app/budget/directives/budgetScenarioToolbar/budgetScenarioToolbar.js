f67937b111d07cbf0cc24b81d3c7b8860cf75134
Mauricio Romero <mromeroval@ucdavis.edu>
2019-04-09 23:17:27 -0700

    Create Budget Instructor List print report and styles

--- app/budget/directives/budgetScenarioToolbar/budgetScenarioToolbar.js
+++ app/budget/directives/budgetScenarioToolbar/budgetScenarioToolbar.js
@@ -72,8 +72,7 @@ let budgetScenarioToolbar = function($window, $location, $routeParams, $rootScop
 			};
 
 			scope.print = function() {
-				window.print();
-				
+				window.print();		
 			};
 
 		} // End Link
