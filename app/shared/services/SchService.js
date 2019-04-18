6b97ecab5275fa4fd633a489eb00e8c3ab995d2f
Jarold Wong <cwjwong@ucdavis.edu>
2019-04-18 12:10:07 -0700

    Remove debugger

--- app/shared/services/SchService.js
+++ app/shared/services/SchService.js
@@ -9,16 +9,19 @@ class SchService {
   constructor() {
     return {
       getUnits: function (course) {
-        if (course.unitsLow > 0) {
-          return course.unitsLow;
-        } else if (course.unitsHigh > 0) {
+        if (course.unitsHigh > 0) {
           return course.unitsHigh;
+        } else if (course.unitsLow > 0) {
+          return course.unitsLow;
+        } else {
+          return 0;
         }
       },
       getSCH: function (enrollment, course) {
-        debugger;
-        if (course.unitsLow > 0) {
-          return (enrollment * course.unitsLow)  || 0;
+        if (course.unitsHigh > 0) {
+          return 0;
+        } else if (course.unitsLow > 0) {
+          return (enrollment * course.unitsLow) || 0;
         } else {
           return 0;
         }
