21cd44ac32a4119531d9925620ab645f7562a8d5
Mauricio Romero <mromeroval@ucdavis.edu>
2019-06-29 11:22:30 -0700

    Fix stylistic issues for print feature

--- app/budget/directives/budgetCosts/budgetCostsPrint/budgetCostsPrint.js
+++ app/budget/directives/budgetCosts/budgetCostsPrint/budgetCostsPrint.js
@@ -3,43 +3,43 @@ import { toCurrency } from 'shared/helpers/string';
 import './budgetCostsPrint.css';
 
 let budgetCostsPrint = function ($rootScope, BudgetActions, TermService) {
-	return {
-		restrict: 'E',
-		template: require('./budgetCostsPrint.html'),
-		replace: true,
-		scope: {
-			state: '<',
-			summary: '<',
-			instructorTypes: '<',
-			selectedBudgetScenario: '<',
-			termNav: '<',
-			scheduleCosts: '<',
-			instructorAssignmentOptions: '<',
-			regularInstructorAssignmentOptions: '<',
-			isLiveDataScenario: '<',
-			lineItems: '<'
-		},
-		link: function (scope){
-			var json = JSON.parse(localStorage.workgroup);
-			scope.workgroupName = json.name;
-			
-			scope.openAddCourseCommentsModal = function(sectionGroupCost) {
-				BudgetActions.openAddCourseCommentsModal(sectionGroupCost);
-			};
+  return {
+    restrict: 'E',
+    template: require('./budgetCostsPrint.html'),
+    replace: true,
+    scope: {
+      state: '<',
+      summary: '<',
+      instructorTypes: '<',
+      selectedBudgetScenario: '<',
+      termNav: '<',
+      scheduleCosts: '<',
+      instructorAssignmentOptions: '<',
+      regularInstructorAssignmentOptions: '<',
+      isLiveDataScenario: '<',
+      lineItems: '<'
+    },
+    link: function (scope){
+      var json = JSON.parse(localStorage.workgroup);
+      scope.workgroupName = json.name;
+      
+      scope.openAddCourseCommentsModal = function(sectionGroupCost) {
+        BudgetActions.openAddCourseCommentsModal(sectionGroupCost);
+      };
 
-			scope.setActiveTerm = function(activeTermTab) {
-				BudgetActions.selectTerm(activeTermTab);
-			};
+      scope.setActiveTerm = function(activeTermTab) {
+        BudgetActions.selectTerm(activeTermTab);
+      };
 
-			scope.getTermName = function(term) {
-				return TermService.getShortTermName(term);
-			};
+      scope.getTermName = function(term) {
+        return TermService.getShortTermName(term);
+      };
 
-			scope.toCurrency = function (value) {
-				return toCurrency(value);
-			};
-		}
-	};
+      scope.toCurrency = function (value) {
+        return toCurrency(value);
+      };
+    }
+  };
 };
 
 export default budgetCostsPrint;
