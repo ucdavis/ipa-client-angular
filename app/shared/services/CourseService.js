77e1a41a8eeb8a462558bc25941d5395fece0f5a
Jarold Wong <cwjwong@ucdavis.edu>
2019-07-19 13:34:09 -0700

    Update logic to include variable units

--- app/shared/services/CourseService.js
+++ app/shared/services/CourseService.js
@@ -9,16 +9,21 @@ class CourseService {
   constructor() {
     return {
       getUnits: function (course) {
-        if (course.unitsHigh > 0) {
-          return course.unitsHigh;
+        if (course.unitsVariable > 0) {
+          return course.unitsVariable;
         } else if (course.unitsLow > 0) {
           return course.unitsLow;
+        } else if (course.unitsHigh > 0) {
+          return course.unitsHigh;
         } else {
           return 0;
         }
       },
       getSCH: function (enrollment, course) {
-        if (course.unitsHigh > 0) {
+        if (course.unitsVariable > 0) {
+          return (enrollment * course.unitsVariable) || 0;
+        } else if (course.unitsHigh > 0) {
+          // variable unit course without unit entry, do not use in SCH calculation
           return 0;
         } else if (course.unitsLow > 0) {
           return (enrollment * course.unitsLow) || 0;
