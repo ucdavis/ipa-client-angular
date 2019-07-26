1c6bcce95baa332f3ea86bb3de3c27cfa49882fa
Jarold Wong <cwjwong@ucdavis.edu>
2019-07-26 10:31:41 -0700

    Clean up comments

--- app/shared/services/CourseService.js
+++ app/shared/services/CourseService.js
@@ -8,13 +8,11 @@
 class CourseService {
   constructor() {
     return {
-      getUnits: function (course, sectionGroup) {
-        // if only one argument, assume sectionGroupCost with unitsVariable property, otherwise use property on sectionGroup
+      getUnits: function(course, sectionGroup) {
         if (sectionGroup) {
           course.unitsVariable = sectionGroup.unitsVariable;
         }
 
-        // if unitsHigh is not null, assume variable unit course
         if (course.unitsHigh > 0) {
           return course.unitsVariable || 0;
         } else if (course.unitsLow > 0) {
@@ -23,18 +21,18 @@ class CourseService {
           return 0;
         }
       },
-      getSCH: function (enrollment, course, sectionGroup) {
+      getSCH: function(enrollment, course, sectionGroup) {
         if (sectionGroup) {
           course.unitsVariable = sectionGroup.unitsVariable;
         }
 
         if (course.unitsVariable > 0) {
-          return (enrollment * course.unitsVariable) || 0;
+          return enrollment * course.unitsVariable || 0;
         } else if (course.unitsHigh > 0) {
-          // variable unit course without unit entry, do not use in SCH calculation
+          // variable unit course without defined units, do not use in SCH calculation
           return 0;
         } else if (course.unitsLow > 0) {
-          return (enrollment * course.unitsLow) || 0;
+          return enrollment * course.unitsLow || 0;
         } else {
           return 0;
         }
