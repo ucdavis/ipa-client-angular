0f773c9df387b0b03bc24e927a8f50fff4ef9104
Mauricio Romero <mromeroval@ucdavis.edu>
2019-08-23 17:52:06 -0700

    Create directives for total teaching balance

---
+++ app/reports/budgetComparison/directives/teachingBalance/teachingBalance.js
@@ -0,0 +1,31 @@
+import { toCurrency } from 'shared/helpers/string';
+
+import './teachingBalance.css';
+
+let teachingBalance = function (StringService) {
+  return {
+    restrict: 'E',
+    template: require('./teachingBalance.html'),
+    replace: true,
+    scope: {
+      year: '<',
+      lineItemCategories: '<',
+      instructorTypes: '<',
+      costs: '<',
+      funding: '<',
+      miscStats: '<',
+      budgetScenarioName: '<'
+    },
+    link: function (scope) {
+      scope.toCurrency = function (value) {
+        return toCurrency(value);
+      };
+
+      scope.toAcademicYear = function (year) {
+        return StringService.toAcademicYear(year);
+      };
+    }
+  };
+};
+
+export default teachingBalance;
---
+++ app/reports/budgetComparison/directives/teachingBalanceChanges/teachingBalanceChanges.js
@@ -0,0 +1,31 @@
+import { toCurrency } from 'shared/helpers/string';
+
+import './teachingBalanceChanges.css';
+
+let teachingBalanceChanges = function (StringService) {
+  return {
+    restrict: 'E',
+    template: require('./teachingBalanceChanges.html'),
+    replace: true,
+    scope: {
+      year: '<',
+      lineItemCategories: '<',
+      instructorTypes: '<',
+      costs: '<',
+      funding: '<',
+      miscStats: '<',
+      budgetScenarioName: '<'
+    },
+    link: function (scope) {
+      scope.toCurrency = function (value) {
+        return toCurrency(value);
+      };
+
+      scope.toAcademicYear = function (year) {
+        return StringService.toAcademicYear(year);
+      };
+    }
+  };
+};
+
+export default teachingBalanceChanges;
