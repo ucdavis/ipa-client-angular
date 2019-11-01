0234e66ff181a7c2386b6565730e5a953508fb61
Mauricio Romero <mromeroval@ucdavis.edu>
2019-11-01 15:04:50 -0700

    Create update function for active filters

--- app/course/directives/courseFilter/courseFilter.js
+++ app/course/directives/courseFilter/courseFilter.js
@@ -8,19 +8,12 @@ let courseFilter = function (CourseActionCreators) {
     scope: {
       filters: '<',
       tags: '<',
-      // instructors: '<',
-      // locations: '<',
-      // schedulingMode: '<'
+      workgroupId: '<',
+      year: '<'
     },
     link: function (scope) {
-      // scope.dayDescriptions = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-      // scope.filterDescriptionMax = 19;
-
-      // scope.toggleCalendarDay = function (index) {
-      //   SchedulingActionCreators.toggleDay(index);
-      // };
-
       scope.toggleTagFilter = function (tagId) {
+        var tagFilters = scope.filters.enabledTagIds;
         var tagIndex = scope.filters.enabledTagIds.indexOf(tagId);
 
         if (tagIndex < 0) {
@@ -29,51 +22,11 @@ let courseFilter = function (CourseActionCreators) {
           scope.filters.enabledTagIds.splice(tagIndex, 1);
         }
 
-        // SchedulingActionCreators.updateTagFilters(scope.filters.enabledTagIds);
+        CourseActionCreators.updateTagFilters(tagFilters);
       };
-
-      // scope.toggleLocationFilter = function (locationId) {
-      //   var locationIndex = scope.filters.enabledLocationIds.indexOf(locationId);
-
-      //   if (locationIndex < 0) {
-      //     scope.filters.enabledLocationIds.push(locationId);
-      //   } else {
-      //     scope.filters.enabledLocationIds.splice(locationIndex, 1);
-      //   }
-
-      //   SchedulingActionCreators.updateLocationFilters(scope.filters.enabledLocationIds);
-      // };
-
-      // scope.toggleInstructorFilter = function (instructorId) {
-      //   var instructorIndex = scope.filters.enabledInstructorIds.indexOf(instructorId);
-
-      //   if (instructorIndex < 0) {
-      //     scope.filters.enabledInstructorIds.push(instructorId);
-      //   } else {
-      //     scope.filters.enabledInstructorIds.splice(instructorIndex, 1);
-      //   }
-
-      //   SchedulingActionCreators.updateInstructorFilters(scope.filters.enabledInstructorIds);
-      // };
-
       scope.unpublishedCoursesToggled = function() {
-        CourseActionCreators.setUnpublishedCoursesFilter(scope.workgroupId, scope.year, !scope.filters.enableUnpublishedCourses)
+        CourseActionCreators.setUnpublishedCoursesFilter(scope.workgroupId, scope.year, !scope.filters.enableUnpublishedCourses);
       };
-
-      // scope.isDayTab = function (tab) {
-      //   switch (tab) {
-      //     case "Sunday":
-      //     case "Monday":
-      //     case "Tuesday":
-      //     case "Wednesday":
-      //     case "Thursday":
-      //     case "Friday":
-      //     case "Saturday":
-      //       return true;
-      //     default:
-      //       return false;
-      //   }
-      // };
     }
   };
 };
