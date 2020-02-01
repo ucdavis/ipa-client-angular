6a8cc493d15d90399924ca84a9c14258e8228f83
Jarold Wong <cwjwong@ucdavis.edu>
2020-01-31 17:42:31 -0800

    Feature/add back to top (#1734)
    * Move backToTop to a shared directive
    
    * Adjust button size and opacity
    
    * Add back to the top button
    
    * Add transition in
    
    * Update class name for fading in button
    
    * Move function to js file
    
    * Remove outdated css class
    
    * Remove extra closing tag

---
+++ app/shared/directives/backToTop/backToTop.js
@@ -0,0 +1,25 @@
+import './backToTop.css';
+
+let backToTop = function($window) {
+  return {
+    restrict: 'E',
+    template: require('./backToTop.html'),
+    replace: true,
+    scope: {},
+    link: function(scope, element) {
+      $window.onscroll = function() {
+        if (this.pageYOffset > this.innerHeight) {
+          element.addClass('backToTop__button--show');
+        } else {
+          element.removeClass('backToTop__button--show');
+        }
+      };
+
+      scope.backToTop = function() {
+        $window.scrollTo(0, 0);
+      };
+    }
+  };
+};
+
+export default backToTop;
