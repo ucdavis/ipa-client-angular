5d82eca5c6196aedfbefb5d75bd58829108dab47
Jarold Wong <cwjwong@ucdavis.edu>
2020-07-15 12:14:44 -0700

    misc clean up

--- app/budget/budgetCtrl.js
+++ app/budget/budgetCtrl.js
@@ -69,52 +69,6 @@ class BudgetCtrl {
         localStorage.removeItem("selectedTerm");
       }
     });
-
-    // _self.$scope.helloButton = function helloButton() {
-    //   _self.BudgetService.downloadWorkgroupScenariosExcel(
-    //       [
-    //         {
-    //           id: 303, // physics 19/20, testing enrollment data
-    //         }
-    //         // {
-    //         //   id: 381,
-    //         // },
-    //         // {
-    //         //   id: 170,
-    //         // }
-    //     ]
-    //   )
-    //   .then(blob => {
-    //       var url = window.URL.createObjectURL(
-    //         new Blob([blob], { type: 'application/vnd.ms-excel' })
-    //       );
-    //       var a = window.document.createElement('a');
-    //       a.href = url;
-    //       a.download = "filename.xlsx";
-    //       window.document.body.appendChild(a); // we need to append the element to the dom -> otherwise it will not work in firefox
-    //       a.click();
-    //       a.remove();  //afterwards we remove the element again
-    //     });
-
-
-      // console.log('Hello');
-      // const headers = new Headers();
-      // headers.append(
-      //   'Authorization',
-      //   'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqaXd1bmciLCJsb2dpbklkIjoiaml3dW5nIiwicmVhbFVzZXJMb2dpbklkIjoiaml3dW5nIiwiZXhwaXJhdGlvbkRhdGUiOjE1OTMyMTI1NDM1OTgsImlhdCI6MTU5MzIwODk0M30.cXfmxDhW4nSlWxFAif_K-fu9iPcAxpU5DWdKeZLKvu0'
-      // );
-      // headers.append('Cookie', 'JSESSIONID=BC14D14911F4CF34E2831E59F71E73CB');
-      // var resp = fetch('http://localhost:8080/api/budgetView/helloworld2', {'method': 'POST', 'headers': headers}).then(response => response.blob())
-      //   .then(blob => {
-      //       var url = window.URL.createObjectURL(blob);
-      //       var a = window.document.createElement('a');
-      //       a.href = url;
-      //       a.download = "filename.xls";
-      //       window.document.body.appendChild(a); // we need to append the element to the dom -> otherwise it will not work in firefox
-      //       a.click();
-      //       a.remove();  //afterwards we remove the element again
-      //   });
-    // };
   }
 }
 
