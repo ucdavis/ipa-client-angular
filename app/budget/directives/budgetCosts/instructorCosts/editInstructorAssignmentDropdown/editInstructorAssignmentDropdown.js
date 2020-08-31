6e0b1e9bbd7fdb4c3d7b77f40a30c3495b8d1d93
Edgar Duran-Perez <eduranperez@ucdavis.edu>
2020-08-31 16:49:29 -0700

    support instructor types on edit dropdown for non live data

--- app/budget/directives/budgetCosts/instructorCosts/editInstructorAssignmentDropdown/editInstructorAssignmentDropdown.js
+++ app/budget/directives/budgetCosts/instructorCosts/editInstructorAssignmentDropdown/editInstructorAssignmentDropdown.js
@@ -12,16 +12,20 @@ let editInstructorAssignmentDropdown = function (BudgetActions) {
 		link: function (scope) {
 			console.log('Edit instructor scope ', scope);
 			scope.setInstructor = function(newInstructor) {
-				console.log('Updating instructor ', scope.instructor);
-					var sectionGroupCost = {
-						id: scope.instructor.id,
-						instructorId: newInstructor.id,
-						cost: parseFloat((scope.instructor.cost || '0.0').toString().replace(/\D/g,'')),
-						reason: scope.instructor.reason,
-						sectionGroupCostId: scope.instructor.sectionGroupCostId,
-						teachingAssingnmentId: scope.instructor.teachingAssignmentId
-					};
-					BudgetActions.updateSectionGroupCostInstructor(sectionGroupCost);
+				var sectionGroupCostInstructor = {
+					id: scope.instructor.id,
+					cost: parseFloat((scope.instructor.cost || '0.0').toString().replace(/\D/g,'')),
+					reason: scope.instructor.reason,
+					sectionGroupCostId: scope.instructor.sectionGroupCostId,
+				};
+				if (newInstructor.isInstructorType){
+					sectionGroupCostInstructor.instructorTypeId = newInstructor.id;
+					sectionGroupCostInstructor.instructorId = null;
+				} else {
+					sectionGroupCostInstructor.instructorId = newInstructor.id,
+					sectionGroupCostInstructor.instructorTypeId = newInstructor.instructorType.id;
+				}
+				BudgetActions.updateSectionGroupCostInstructor(sectionGroupCostInstructor);
 			};
 		}
 	};
