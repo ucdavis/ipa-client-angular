fd4c20d9a9b9ee58c011837f02ce890a8a250fae
Edgar Duran-Perez <eduranperez@ucdavis.edu>
2020-09-09 15:42:56 -0700

    only show sync if not a subset and only add non dup instructors or instructor types if count does not match

--- app/budget/directives/budgetCosts/courseCostsRow/courseCostsRow.js
+++ app/budget/directives/budgetCosts/courseCostsRow/courseCostsRow.js
@@ -2,7 +2,7 @@ import { toCurrency } from 'shared/helpers/string';
 
 import './courseCostsRow.css';
 
-let courseCostsRow = function ($rootScope, BudgetActions) {
+let courseCostsRow = function ($rootScope, BudgetActions, BudgetReducers) {
 	return {
 		restrict: 'A',
 		template: require('./courseCostsRow.html'),
@@ -71,18 +71,61 @@ let courseCostsRow = function ($rootScope, BudgetActions) {
 				scope.sectionGroupCost.instructorTypeId = scope.sectionGroupCost.sectionGroup.assignedInstructorType ? scope.sectionGroupCost.sectionGroup.assignedInstructorType.id : null;
 				BudgetActions.updateSectionGroupCost(scope.sectionGroupCost);
 				if (scope.sectionGroupCost.sectionGroup){
-					var currentInstructorIds = scope.sectionGroupCost.sectionGroupCostInstructors.map(function(instructor){
+
+					var sectionGroupCostInstructors = (BudgetReducers._state.sectionGroupCostInstructors.bySectionGroupCostId[scope.sectionGroupCost.id] || []);
+					var currentInstructorIds = sectionGroupCostInstructors.map(function(instructor){
 						return instructor.instructorId;
 					});
+
+					var currentTypeIdsCount = {};
+					for (var sectionGroupCostInstructor of sectionGroupCostInstructors){
+						if (!sectionGroupCostInstructor.instructorId){
+							if (currentTypeIdsCount[sectionGroupCostInstructor.instructorTypeId]){
+								currentTypeIdsCount[sectionGroupCostInstructor.instructorTypeId] += 1;
+							} else {
+								currentTypeIdsCount[sectionGroupCostInstructor.instructorTypeId] = 1;
+							}
+						}
+					}
+
 					const instructors = scope.sectionGroupCost.sectionGroup.assignedInstructors.map(function(liveDataInstructor){
 						return {
 							instructorId: liveDataInstructor.id,
 							instructorTypeId: liveDataInstructor.instructorTypeId,
 							sectionGroupCostId: scope.sectionGroupCost.id
 						};
-					}).filter(instructor => !currentInstructorIds.includes(instructor.instructorId));
-					BudgetActions.createSectionGroupCostInstructors(instructors);
+					}).filter(instructor => (!currentInstructorIds.includes(instructor.instructorId) && (instructor.instructorId ? true : !currentTypeIdsCount[instructor.instructorTypeId] || currentTypeIdsCount[instructor.instructorTypeId]-- < 1)));
+					if (instructors.length > 0){
+						BudgetActions.createSectionGroupCostInstructors(instructors);
+					}
 				}
+			};
+
+			scope.isSubset = function() {
+				if (scope.sectionGroupCost.sectionGroup){
+
+					var sectionGroupCostInstructors = (BudgetReducers._state.sectionGroupCostInstructors.bySectionGroupCostId[scope.sectionGroupCost.id] || []);
+					var currentInstructorIds = sectionGroupCostInstructors.map(function(instructor){
+						return instructor.instructorId;
+					});
+
+					var currentTypeIdsCount = {};
+					for (var sectionGroupCostInstructor of sectionGroupCostInstructors){
+						if (!sectionGroupCostInstructor.instructorId){
+							if (currentTypeIdsCount[sectionGroupCostInstructor.instructorTypeId]){
+								currentTypeIdsCount[sectionGroupCostInstructor.instructorTypeId] += 1;
+							} else {
+								currentTypeIdsCount[sectionGroupCostInstructor.instructorTypeId] = 1;
+							}
+						}
+					}
+
+					const instructors = scope.sectionGroupCost.sectionGroup.assignedInstructors.filter(instructor => (!currentInstructorIds.includes(instructor.id) && (instructor.id ? true : !currentTypeIdsCount[instructor.instructorTypeId] || currentTypeIdsCount[instructor.instructorTypeId]-- < 1)));
+					if (instructors.length === 0){
+						return true;
+					}
+				}
+				return false;
 			};
 		}
 	};
