71c26533e09ccb90dd16139e3beddafbb8b09e78
Edgar Duran-Perez <eduranperez@ucdavis.edu>
2020-09-24 16:33:39 -0700

    transition to one cost directive.  support non live data for reason category

--- app/budget/budgetApp.js
+++ app/budget/budgetApp.js
@@ -38,7 +38,6 @@ import budgetCostsPrint from './directives/budgetCosts/budgetCostsPrint/budgetCo
 import courseCostsRow from './directives/budgetCosts/courseCostsRow/courseCostsRow.js';
 import instructorCostsRow from './directives/budgetCosts/instructorCostsRow/instructorCostsRow.js';
 import instructorAssignmentDropdown from './directives/budgetCosts/instructorCostsRow/instructorAssignmentDropdown/instructorAssignmentDropdown.js';
-import instructorCostsLiveData from './directives/budgetCosts/instructorCostsLiveData/instructorCostsLiveData.js';
 import instructorCosts from './directives/budgetCosts/instructorCosts/instructorCosts.js';
 import addInstructorCost from './directives/budgetCosts/addInstructorCost/addInstructorCost.js';
 import addInstructorAssignmentDropdown from './directives/budgetCosts/addInstructorCost/addInstructorAssignmentDropdown/addInstructorAssignmentDropdown.js';
@@ -115,7 +114,6 @@ const budgetApp = angular // eslint-disable-line no-undef
   .directive("courseCostsRow", courseCostsRow)
   .directive("instructorCostsRow", instructorCostsRow)
   .directive("instructorAssignmentDropdown", instructorAssignmentDropdown)
-  .directive("instructorCostsLiveData", instructorCostsLiveData)
   .directive("instructorCosts", instructorCosts)
   .directive("addInstructorCost", addInstructorCost)
   .directive("addInstructorAssignmentDropdown", addInstructorAssignmentDropdown)
