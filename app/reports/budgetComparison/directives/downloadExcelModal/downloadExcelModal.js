696640c32befa3fd4d9963e9b13ab05a6be8b6f2
Jarold Wong <cwjwong@ucdavis.edu>
2020-10-02 16:33:05 -0700

    [rtt-111] budget budgetComparison download enhancements (#1791)
    * sort departments by name
    
    * save scenario selections
    
    * add download selection checkbox
    
    * add download selection checkbox
    
    * add toggle all option
    
    * add toggle all downloads option
    
    * triple equals
    
    * add reset selections
    
    * remove extra div
    
    * add reset selections button
    
    * refactor reset selections function
    
    * match budget download modal style
    
    * add option to sort by recent changes
    
    * save sort option
    
    * add last active text

--- app/reports/budgetComparison/directives/downloadExcelModal/downloadExcelModal.js
+++ app/reports/budgetComparison/directives/downloadExcelModal/downloadExcelModal.js
@@ -1,4 +1,6 @@
 import './downloadExcelModal.css';
+import { _array_sortByProperty } from '../../../../shared/helpers/array';
+import { dateToCalendar } from '../../../../shared/helpers/dates';
 
 let downloadExcelModal = function (BudgetComparisonReportActions, BudgetComparisonReportService) {
   return {
@@ -14,6 +16,7 @@ let downloadExcelModal = function (BudgetComparisonReportActions, BudgetComparis
       scope.status = null;
       scope.previousYear = (parseInt(localStorage.getItem('year')) - 1).toString().yearToAcademicYear();
       scope.currentYear = localStorage.getItem('year').yearToAcademicYear();
+      scope.isSortedByRecentActivity = false;
 
       // {
       //   "DSS": {
@@ -26,29 +29,81 @@ let downloadExcelModal = function (BudgetComparisonReportActions, BudgetComparis
       //   }
       // }
       scope.$watch('userWorkgroupsScenarios', function(userWorkgroupsScenarios) {
-        if (userWorkgroupsScenarios) {
-          scope.departmentScenarios = Object.keys(
-            userWorkgroupsScenarios
-          ).map((department) => ({
-            name: department,
-            current: userWorkgroupsScenarios[department].current,
-            previous: userWorkgroupsScenarios[department].previous,
-            selectedPrevious: `${(userWorkgroupsScenarios[department].previous.find(scenario => scenario.name === 'Live Data') || {}).id}`,
-            selectedCurrent: `${(userWorkgroupsScenarios[department].current.find(scenario => scenario.name === 'Live Data') || {}).id}`
-          }));
+        if (localStorage.getItem("budgetComparisonDownloadSelections")) {
+          scope.departmentScenarios = JSON.parse(localStorage.getItem("budgetComparisonDownloadSelections"));
+          scope.isSortedByRecentActivity = JSON.parse(localStorage.getItem("budgetComparisonDownloadSorted"));
+
+          scope.downloadAllDepartments = scope.departmentScenarios.every(department => department.download === true);
+        } else if (userWorkgroupsScenarios) {
+          scope.departmentScenarios = Object.keys(userWorkgroupsScenarios)
+            .sort()
+            .map((department) => ({
+              name: department,
+              current: userWorkgroupsScenarios[department].current,
+              previous: userWorkgroupsScenarios[department].previous,
+              selectedPrevious: `${(userWorkgroupsScenarios[department].previous.find(scenario => scenario.fromLiveData === true) || {}).id}`,
+              selectedCurrent: `${(userWorkgroupsScenarios[department].current.find(scenario => scenario.fromLiveData === true) || {}).id}`,
+              lastModifiedOn: Math.max(...scope.userWorkgroupsScenarios[department].current.map(scenario => scenario.lastModifiedOn)),
+              download: true
+            }));
+
+            scope.downloadAllDepartments = true;
         }
       }, true);
+
+      scope.sortDepartmentsByRecentActivity = function() {
+        if (scope.isSortedByRecentActivity === false) {
+          scope.isSortedByRecentActivity = true;
+          scope.departmentScenarios = _array_sortByProperty(scope.departmentScenarios, "lastModifiedOn", true);
+        } else {
+          scope.isSortedByRecentActivity = false;
+          scope.departmentScenarios = _array_sortByProperty(scope.departmentScenarios, "name");
+        }
+      };
+
+      scope.resetDownloadSelections = function() {
+        scope.departmentScenarios.forEach(departmentScenarios => {
+          departmentScenarios.download = true;
+          departmentScenarios.selectedPrevious = `${departmentScenarios.previous.find(scenario => scenario.fromLiveData === true).id}`;
+          departmentScenarios.selectedCurrent = `${departmentScenarios.current.find(scenario => scenario.fromLiveData === true).id}`;
+        });
+        scope.downloadAllDepartments = true;
+      };
+
+      scope.toggleAllDepartmentDownloads = function() {
+        if (scope.downloadAllDepartments) {
+          scope.departmentScenarios.forEach(department => department.download = false);
+          scope.downloadAllDepartments = false;
+        } else {
+          scope.departmentScenarios.forEach(department => department.download = true);
+          scope.downloadAllDepartments = true;
+        }
+      };
+
+      scope.toggleDepartmentDownload = function (department) {
+        department.download = !department.download;
+
+        scope.downloadAllDepartments = scope.departmentScenarios.every(department => department.download === true);
+      };
 
       scope.close = function () {
         scope.status = null;
         BudgetComparisonReportActions.toggleDownloadModal();
+        localStorage.setItem("budgetComparisonDownloadSelections", JSON.stringify(scope.departmentScenarios));
+        localStorage.setItem("budgetComparisonDownloadSorted", JSON.stringify(scope.isSortedByRecentActivity));
+      };
+
+      scope.dateToCalendar = function (date) {
+        return dateToCalendar(date);
       };
 
       scope.submit = function () {
         scope.isDisabled = true;
-        let scenarioIdPairs = scope.departmentScenarios.map((scenario) => [
-          { id: parseInt(scenario.selectedPrevious) },
-          { id: parseInt(scenario.selectedCurrent) },
+        let scenarioIdPairs = scope.departmentScenarios
+          .filter(department => department.download)
+          .map((department) => [
+          { id: parseInt(department.selectedPrevious) },
+          { id: parseInt(department.selectedCurrent) },
         ]);
 
         BudgetComparisonReportService.downloadBudgetComparisonExcel(scenarioIdPairs).then((res) => {
