cfd582c9757ca20b4353026bb2f6e5fe3e1c2f90
Edgar Duran-Perez <eduranperez@ucdavis.edu>
2021-01-27 09:23:41 -0800

    ensure new offering would be unique

--- app/course/directives/convertSectionsModal/convertSectionsModal.js
+++ app/course/directives/convertSectionsModal/convertSectionsModal.js
@@ -42,6 +42,7 @@ let convertSectionsModal = function (CourseActionCreators) {
         if (scope.selectedEntity && scope.selectedEntity.sequencePattern){
           if (!scope.isSeries()){
             if (scope.selectedEntity.sequencePattern.length === 1 && isLetter(scope.selectedEntity.sequencePattern[0].toUpperCase())){
+              scope.selectedEntity.sequencePatternTooltipMessage = null;
               isValid = true;
             } else {
               scope.selectedEntity.sequencePatternTooltipMessage = "Sequence pattern format is incorrect. Valid format is '1 letter' (ex: 'A').";
@@ -54,6 +55,7 @@ let convertSectionsModal = function (CourseActionCreators) {
               isNumber(scope.selectedEntity.sequencePattern[1]) &&
               isNumber(scope.selectedEntity.sequencePattern[2])
             ){
+              scope.selectedEntity.sequencePatternTooltipMessage = null;
               isValid = true;
             } else {
               scope.selectedEntity.sequencePatternTooltipMessage = "Sequence pattern format is incorrect. Valid format is '3 numbers' (ex: '002').";
@@ -62,6 +64,34 @@ let convertSectionsModal = function (CourseActionCreators) {
           }
         }
         return isValid;
+      };
+
+      scope.isUnique = function () {
+        var isUnique = false;
+        if (scope.selectedEntity && scope.selectedEntity.sequencePattern){
+          var sectionGroup = scope.selectedEntity;
+          var course = course = scope.state.courses.list[sectionGroup.courseId];
+          var courseDescription = course.subjectCode + "-" + course.courseNumber + "-" + sectionGroup.sequencePattern;
+          isUnique = true;
+          scope.selectedEntity.sequencePatternTooltipMessage = null;
+
+          scope.state.courses.ids.forEach(function(courseId) {
+            var c = scope.state.courses.list[courseId];
+            let cDescription = c.subjectCode + "-" + c.courseNumber + "-" + c.sequencePattern;
+
+            if (courseDescription == cDescription) {
+              scope.state.sectionGroups.ids.forEach(function(sectionGroupId) {
+                var sg = scope.state.sectionGroups.list[sectionGroupId];
+                if(sg.courseId == c.id && sg.termCode == sectionGroup.termCode){
+                  scope.selectedEntity.sequencePatternTooltipMessage = "Sequence pattern already in use.";
+                  isUnique = false;
+                }
+              });
+            }
+          });
+      }
+
+        return isUnique;
       };
     } // end link
   };
