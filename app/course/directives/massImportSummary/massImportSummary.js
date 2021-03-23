36f7d80dee5c67f321bc0a6877fa2b2540bab177
Jarold Wong <cwjwong@ucdavis.edu>
2021-03-23 15:31:50 -0700

    Revert "use existing course if effectiveTermCode differs"
    This reverts commit 7535ad86304b77f40c19eaa8029c52c69ff53776.

--- app/course/directives/massImportSummary/massImportSummary.js
+++ app/course/directives/massImportSummary/massImportSummary.js
@@ -8,17 +8,13 @@ let massImportSummary = function (CourseActionCreators) {
        * Sends the selected courses to IPA-WEB to be imported to the schedule
        */
       scope.importCoursesAndSectionGroups = function () {
-        var selectedCourses = scope.view.state.courses.importList.filter(function (c) { return c.import;});
+        var selectedCourseIds = scope.view.state.courses.importList
+          .filter(function (c) { return c.import;})
+          .map(function (c) { return c.subjectCode + c.courseNumber + c.sequencePattern + c.effectiveTermCode; });
 
         var sectionGroupImports = scope.view.state.sectionGroups.importList.filter(function (sg) {
-          const course = selectedCourses.find(c => c.subjectCode === sg.subjectCode && c.courseNumber === sg.courseNumber && c.sequencePattern === sg.sequencePattern);
-
-          // use existing course on the schedule if SG effectiveTermCode differs
-          if (course !== undefined && sg.effectiveTermCode !== course.effectiveTermCode) {
-            sg.effectiveTermCode = course.effectiveTermCode;
-          }
-
-          return course !== undefined;
+          var sgCourseId = sg.subjectCode + sg.courseNumber + sg.sequencePattern + sg.effectiveTermCode;
+          return selectedCourseIds.indexOf(sgCourseId) >= 0;
         });
 
         scope.view.state.uiState.massImportInProgress = true;
@@ -28,10 +24,10 @@ let massImportSummary = function (CourseActionCreators) {
 
         if (scope.view.state.uiState.massImportSource == 'IPA') {
           CourseActionCreators.importCoursesAndSectionGroupsFromIPA(
-            sectionGroupImports, scope.workgroupId, scope.year, selectedCourses.length, importTimes, importAssignments);
+            sectionGroupImports, scope.workgroupId, scope.year, selectedCourseIds.length, importTimes, importAssignments);
         } else {
           CourseActionCreators.importCoursesAndSectionGroups(
-            sectionGroupImports, scope.workgroupId, scope.year, selectedCourses.length, importTimes, importAssignments);
+            sectionGroupImports, scope.workgroupId, scope.year, selectedCourseIds.length, importTimes, importAssignments);
         }
       };
     }
