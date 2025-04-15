430e7e626ba3bb082655812b6ebf9a3f6b30efe7
Jarold Wong <cwjwong@ucdavis.edu>
2025-04-15 13:34:49 -0700

    add bulk update for banner values

--- app/registrarReconciliationReport/controllers/registrarReconciliationReportCtrl.js
+++ app/registrarReconciliationReport/controllers/registrarReconciliationReportCtrl.js
@@ -6,14 +6,14 @@
  * Controller of the ipaClientAngularApp
  */
 class registrarReconciliationReportCtrl {
-	constructor ($scope, $rootScope, $route, $routeParams, Term, registrarReconciliationReportActionCreators, AuthService, validate) {
+	constructor ($scope, $rootScope, $route, $routeParams, Term, RegistrarReconciliationReportActionCreators, AuthService, validate) {
 		var self = this;
 		this.$scope = $scope;
 		this.$rootScope = $rootScope;
 		this.$routeParams = $routeParams;
 		this.$route = $route;
 		this.Term = Term;
-		this.registrarReconciliationReportActionCreators = registrarReconciliationReportActionCreators;
+		this.registrarReconciliationReportActionCreators = RegistrarReconciliationReportActionCreators;
 		this.authService = AuthService;
 		$scope.noAccess = validate ? validate.noAccess : null;
 		$scope.sharedState = $scope.sharedState || AuthService.getSharedState();
@@ -26,6 +26,9 @@ class registrarReconciliationReportCtrl {
 		this.$scope.workgroupId = this.$routeParams.workgroupId;
 		this.$scope.year = this.$routeParams.year;
 		this.$scope.termShortCode = this.$routeParams.termShortCode;
+		this.$scope.pendingUpdates = [];
+		this.$scope.showUpdatesModal = false;
+		this.$scope.showBulkUpdateButton = false;
 
 		if (!this.$scope.termShortCode) {
 			// LINTME
@@ -48,6 +51,10 @@ class registrarReconciliationReportCtrl {
 
 			generateFilteredDisplay(data);
 
+			self.$scope.showBulkUpdateButton = Object.values(self.$scope.view.state.sections.list).some(
+				(section) => section.dwChanges || (section.id && (section.noLocal === false || section.noRemote === false))
+			);
+
 			self.$scope.view.hasAccess = self.$scope.sharedState.currentUser.isAdmin() ||
 			self.$scope.sharedState.currentUser.hasRole('academicPlanner', self.$scope.sharedState.workgroup.id);
 		});
@@ -69,6 +76,97 @@ class registrarReconciliationReportCtrl {
 		}
 
 		this.$scope.nextShortTermCode = this.$scope.allTerms[index];
+
+		this.$scope.acceptBannerUpdates = function () {
+			let updates = [];
+			
+			Object.values(this.view.state.sections.list)
+				.filter((section) => section.dwHasChanges)
+				.forEach((section) => {
+					section.instructors.forEach((instructor) => {
+						if (instructor.noRemote) {
+							updates.push({
+								section: section,
+								property: 'unassign',
+								instructor: instructor,
+								uniqueKey: section.uniqueKey,
+								isChecked: true,
+							});
+						}
+						
+						if (instructor.noLocal) {
+							updates.push({
+								section: section,
+								property: 'assign',
+								instructor: instructor,
+								uniqueKey: section.uniqueKey,
+								isChecked: true,
+							});
+						}
+					});
+
+					// activity changes
+					section.activities.forEach((activity) => {
+						if (activity.dwChanges) {
+							Object.entries(activity.dwChanges).forEach(([property, { value }]) => {
+								updates.push({
+									section: section,
+									activity: activity,
+									property: property,
+									value: value,
+									uniqueKey: section.uniqueKey,
+									isChecked: true,
+								});
+							});
+						}
+						});
+
+					// property changes
+					if (section.dwChanges) {
+						Object.entries(section.dwChanges).forEach(([property, { value }]) => {
+							updates.push({
+								section: section,
+								property: property,
+								value: value,
+								uniqueKey: section.uniqueKey,
+								isChecked: true,
+							});
+						});
+					}
+				});
+
+			self.$scope.pendingUpdates = updates;
+			self.$scope.showUpdatesModal = true;
+		};
+
+		this.$scope.closeUpdatesModal = function () {
+			self.$scope.pendingUpdates = [];
+			self.$scope.showUpdatesModal = false;
+		};
+
+		this.$scope.applyUpdates = async function () {
+			self.$scope.showUpdatesModal = false;
+
+			const updateActions = {
+				crn: (update) => self.registrarReconciliationReportActionCreators.updateSection({ id: update.section.id, crn: update.value }, 'crn', update.uniqueKey),
+				seats: (update) => self.registrarReconciliationReportActionCreators.updateSection({ id: update.section.id, seats: update.value }, 'seats', update.uniqueKey),
+				dayIndicator: (update) => self.registrarReconciliationReportActionCreators.updateActivity({ id: update.activity.id, typeCode: update.activity.typeCode, dayIndicator: update.value }, 'dayIndicator'),
+				startTime: (update) => self.registrarReconciliationReportActionCreators.updateActivity({ id: update.activity.id, typeCode: update.activity.typeCode, startTime: update.value ? moment(update.value, "HHmm").format("HH:mm:ss") : null }, 'startTime'), // eslint-disable-line no-undef
+				endTime: (update) => self.registrarReconciliationReportActionCreators.updateActivity({ id: update.activity.id, typeCode: update.activity.typeCode, endTime: update.value ? moment(update.value, "HHmm").format("HH:mm:ss") : null }, 'endTime'), // eslint-disable-line no-undef
+				bannerLocation: (update) => self.registrarReconciliationReportActionCreators.updateActivity({ id: update.activity.id, typeCode: update.activity.typeCode, bannerLocation: update.value }, 'bannerLocation'),
+				unassign: (update) => self.registrarReconciliationReportActionCreators.unAssignInstructor(update.section, update.instructor),
+				assign: (update) => self.registrarReconciliationReportActionCreators.assignInstructor(update.section, update.instructor),
+			};
+
+			for (const update of self.$scope.pendingUpdates) {
+				if (update.isChecked && updateActions[update.property]) {
+					updateActions[update.property](update);
+					await new Promise(resolve => setTimeout(resolve, 200)); // Add a delay between each update
+				}
+
+				self.$scope.pendingUpdates = [];
+			}
+		};
 
 		for (var i = 0; i < this.$scope.allTerms.length; i++) {
 			let shortTermCode = this.$scope.allTerms[i];
